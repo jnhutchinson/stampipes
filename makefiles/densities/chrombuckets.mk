eac0fc9e3c5851ac944545d9e3a1be878c9ea434
Audra Johnson <audrakj@uw.edu>
2014-07-28 13:09:22 -0700

    Density creation makefiles

---
+++ makefiles/densities/chrombuckets.mk
@@ -0,0 +1,27 @@
+###################
+# This creates the chromosome buckets to use for making densities to make bigWig files
+# for the UCSC browser.
+# Needs to be given either BWAINDEX with a .fai or a FAI file.
+###################
+# BWAINDEX=/path/to/genome/hg19/hg19
+# GENOME=hg19
+###################
+# REQUIRED MODULES
+###################
+# module load bedops
+###################
+
+FAI ?= $(BWAINDEX).fai
+# WINDOW
+WIN ?= 75
+# BIN INTERVAL
+BINI ?= 20
+BUCKETS_DIR ?= $(STAMPIPES)/data/densities
+BUCKETS_FILE ?= $(BUCKETS_DIR)/chrom-buckets.$(GENOME).$(WIN).$(BINI).bed.starch
+
+all : $(BUCKETS_FILE)
+
+$(BUCKETS_FILE) :
+	awk -v w=$(WIN) '{if(NR>1){print $$1"\t0\t"$$2-w}}' $(FAI) | sort-bed - \
+      | awk -v binI=$(BINI) -v win=$(WIN) '{ for(i = $$2 + win; i < $$3; i += binI) { print $$1"\t"i - win"\t"i + win }}' \
+      | starch - > $(BUCKETS_DIR)/chrom-buckets.$(GENOME).$(WIN).$(BINI).bed.starch
---
+++ makefiles/densities/density.mk
@@ -0,0 +1,58 @@
+###################
+# This creates BigWig files from a BAM file for loading into a GCSC browser.
+# 
+###################
+# SAMPLE_NAME=Example_NoIndex_L007
+# BWAINDEX=/path/to/genome/hg19/hg19
+# GENOME=hg19
+# READLENGTH=36
+###################
+# Optional arguments
+###################
+# WIN=75
+# BIN=20
+# STAMPIPES=~/stampipes # Location of stampipes installation
+###################
+# REQUIRED MODULES
+###################
+# module load samtools
+# module load python
+# module load bedops
+# module load bedtools
+###################
+
+FAI ?= $(BWAINDEX).fai
+BAMFILE ?= $(SAMPLE_NAME).uniques.sorted.bam
+STAMPIPES ?= ~/stampipes
+
+# Window
+WIN=75
+# Bin interval
+BINI=20
+TAGEXP=$(FLOWCELL)-$(SAMPLE_NAME)-$(FORG)
+
+BUCKETS_DIR ?= $(STAMPIPES)/data/densities/
+
+CHROM_BUCKET ?= $(BUCKETS_DIR)/chrom-buckets.$(GENOME).$(WIN).$(BINI).bed.starch
+
+all : $(SAMPLE_NAME).$(WIN).$(BINI).uniques-density.$(READLENGTH).$(GENOME).bed.starch $(SAMPLE_NAME).$(WIN)_$(BINI).$(GENOME).bw
+
+# clip tags to single 5' end base
+$(SAMPLE_NAME).m$(READLENGTH).uniques.$(BINI).bed : $(BAMFILE)
+	bamToBed -i $^ \
+    | awk '{if($$6=="+"){s=$$2; e=$$2+1}else{s=$$3-1; e=$$3}print $$1"\t"s"\t"e}' \
+    | sort-bed - \
+    > $@
+
+$(SAMPLE_NAME).$(WIN).$(BINI).uniques-density.$(READLENGTH).$(GENOME).bed.starch : $(CHROM_BUCKET) $(SAMPLE_NAME).m$(READLENGTH).uniques.$(BINI).bed   
+	unstarch $(CHROM_BUCKET) \
+    | bedmap --faster --range $(WIN) --echo --count --delim "\t" - $(SAMPLE_NAME).m$(READLENGTH).uniques.$(BINI).bed \
+    | starch - \
+    > $@
+
+$(SAMPLE_NAME).$(WIN)_$(BINI).$(GENOME).wig : $(SAMPLE_NAME).$(WIN).$(BINI).uniques-density.$(READLENGTH).$(GENOME).bed.starch
+	unstarch $^ | awk -v binI=$(BINI) -f $(STAMPIPES)/awk/bedToWig.awk > $@
+      
+$(SAMPLE_NAME).$(WIN)_$(BINI).$(GENOME).bw : $(SAMPLE_NAME).$(WIN)_$(BINI).$(GENOME).wig
+	wigToBigWig -clip $^ $(FAI) $@
+
