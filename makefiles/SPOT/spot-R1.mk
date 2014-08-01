1f6798756099fea37ecdd706375e7d8b0c5d27e6
Audra Johnson <audrakj@uw.edu>
2014-08-01 02:52:30 -0700

    Refine SPOT scripts

--- makefiles/SPOT/spot-R1.mk
+++ makefiles/SPOT/spot-R1.mk
@@ -22,18 +22,17 @@ FAI ?= $(BWAINDEX).fai
 SAMPLE_SIZE ?= 5000000
 BAMFILE ?= $(SAMPLE_NAME).uniques.sorted.bam
 STAMPIPES ?= ~/stampipes
-PICARD_DIR ?= ~/picard-tools-1.114
 HOTSPOT_DIR ?= ~/hotspot/hotspot-distr
 
-TMPDIR ?= .
-OUTDIR ?= .
+TMPDIR ?= $(shell pwd)
+OUTDIR ?= $(shell pwd)
 
 SPOTDIR ?= $(TMPDIR)/$(SAMPLE_NAME)_spot_R1
 
 all : calcdup calcspot
 
 SPOT_OUT ?= $(OUTDIR)/$(SAMPLE_NAME).R1.rand.uniques.sorted.spot.out
-DUP_OUT ?= $(OUTDIR)/$(SAMPLE_LABEL).R1.rand.uniques.sorted.spotdups.txt
+DUP_OUT ?= $(OUTDIR)/$(SAMPLE_NAME).R1.rand.uniques.sorted.spotdups.txt
 
 RANDOM_SAMPLE_BAM ?= $(TMPDIR)/$(SAMPLE_NAME).R1.rand.uniques.sorted.bam
 
@@ -61,8 +60,8 @@ $(SPOTDIR)/$(SAMPLE_NAME).R1.rand.uniques.sorted.spot.out : $(RANDOM_SAMPLE_BAM)
 
 # Calculate the duplication score of the random sample
 $(DUP_OUT) : $(RANDOM_SAMPLE_BAM)
-	bash -e $(STAMPIPES)/scripts/SPOT/picard_dups.bash $(RANDOM_SAMPLE_BAM) $(TMPDIR)/$(SAMPLE_NAME).R1.rand.uniques.dup $(DUP_OUT) $(PICARD_DIR)
+#	bash -e $(STAMPIPES)/scripts/SPOT/picard_dups.bash $(RANDOM_SAMPLE_BAM) $(TMPDIR)/$(SAMPLE_NAME).R1.rand.uniques.dup $(DUP_OUT)
 # This should be the rule but qmake errors out with
 # Error: Unable to access jarfile ~/picard-tools-1.114/MarkDuplicates.jar
-# java -jar $(PICARD_DIR)/MarkDuplicates.jar INPUT=$(SAMPLE_NAME).R1.rand.uniques.sorted.bam OUTPUT=$(SAMPLE_NAME).R1.rand.uniques.dup \
-#    METRICS_FILE=$(SAMPLE_NAME).R1.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
+	java -jar `which MarkDuplicates.jar` INPUT=$(SAMPLE_NAME).R1.rand.uniques.sorted.bam OUTPUT=$(SAMPLE_NAME).R1.rand.uniques.dup \
+	  METRICS_FILE=$(SAMPLE_NAME).R1.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
--- makefiles/SPOT/spot-Rboth.mk
+++ makefiles/SPOT/spot-Rboth.mk
@@ -27,15 +27,15 @@ HOTSPOT_DIR ?= ~/hotspot/hotspot-distr
 
 SAMTOOLS ?= samtools
 
-TMPDIR ?= .
-OUTDIR ?= .
+TMPDIR ?= $(shell pwd)
+OUTDIR ?= $(shell pwd)
 
 SPOTDIR ?= $(TMPDIR)/$(SAMPLE_NAME)_spot_Rboth
 
 all : calcdup calcspot
 
 SPOT_OUT ?= $(OUTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spot.out
-DUP_OUT ?= $(OUTDIR)/$(SAMPLE_LABEL).Rboth.rand.uniques.sorted.spotdups.txt
+DUP_OUT ?= $(OUTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spotdups.txt
 
 RANDOM_SAMPLE_BAM ?= $(TMPDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.bam
 
@@ -63,8 +63,8 @@ $(SPOTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spot.out : $(RANDOM_SAMPLE_B
 
 # Calculate the duplication score of the random sample
 $(DUP_OUT) : $(RANDOM_SAMPLE_BAM)
-	bash -e $(STAMPIPES)/scripts/SPOT/picard_dups.bash $(RANDOM_SAMPLE_BAM) $(TMPDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.dup $(DUP_OUT) $(PICARD_DIR)
+#	bash -e $(STAMPIPES)/scripts/SPOT/picard_dups.bash $(RANDOM_SAMPLE_BAM) $(TMPDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.dup $(DUP_OUT) $(PICARD_DIR)
 # This should be the rule but qmake errors out with
 # Error: Unable to access jarfile ~/picard-tools-1.114/MarkDuplicates.jar
-# java -jar $(PICARD_DIR)/MarkDuplicates.jar INPUT=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.bam OUTPUT=$(SAMPLE_NAME).Rboth.rand.uniques.dup \
-#    METRICS_FILE=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
+	java -jar `MarkDuplicates.jar` INPUT=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.bam OUTPUT=$(SAMPLE_NAME).Rboth.rand.uniques.dup \
+	   METRICS_FILE=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
