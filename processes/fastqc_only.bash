830330f27b640926d29eb760a62f27fbf0ffb30b
Audra Johnson <audrakj@uw.edu>
2014-09-01 15:00:25 -0700

    Hide qsub logs for fastqc only jobs

--- processes/fastqc_only.bash
+++ processes/fastqc_only.bash
@@ -1,6 +1,6 @@
 if [ ! -e ${SAMPLE_NAME}_R1_fastqc -o ! -e ${SAMPLE_NAME}_R2_fastqc ]; then
 
-qsub -N "fq${SAMPLE_NAME}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub -N ".fq${SAMPLE_NAME}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
   echo "Hostname: " `hostname`
   
