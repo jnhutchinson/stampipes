474eae88fe437f753e973fb4cb553829ff5b3653
Audra Johnson <audrakj@uw.edu>
2014-09-08 12:51:04 -0700

    Sync spot-Rboth method with spot-R1 method

--- makefiles/SPOT/spot-Rboth.mk
+++ makefiles/SPOT/spot-Rboth.mk
@@ -61,7 +61,17 @@ $(SPOT_OUT) : $(SPOTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spot.out
 $(SPOTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spot.out : $(RANDOM_SAMPLE_BAM)
 	bash -e $(STAMPIPES)/scripts/SPOT/runhotspot.bash $(HOTSPOT_DIR) $(SPOTDIR) $(RANDOM_SAMPLE_BAM) $(GENOME) $(READLENGTH) $(ASSAY)
 
+# picard has trouble calculating dups when we are just working with an
+# unpaired sampling on paired end data; converting back and forth with
+# BED will strip that information
+$(RANDOM_SAMPLE_BAM).bed : $(RANDOM_SAMPLE_BAM)
+	bamToBed -i $^ > $@
+
+$(RANDOM_SAMPLE_BAM).bed.sorted.bam : $(RANDOM_SAMPLE_BAM).bed
+	bedToBam -i $^ -g $(FAI) > $(RANDOM_SAMPLE_BAM).bed.bam
+	samtools sort $(RANDOM_SAMPLE_BAM).bed.bam $(RANDOM_SAMPLE_BAM).bed.sorted
+
 # Calculate the duplication score of the random sample
-$(DUP_OUT) : $(RANDOM_SAMPLE_BAM)
-	java -jar `MarkDuplicates.jar` INPUT=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.bam OUTPUT=$(SAMPLE_NAME).Rboth.rand.uniques.dup \
-	   METRICS_FILE=$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
+$(DUP_OUT) : $(RANDOM_SAMPLE_BAM).bed.sorted.bam
+	java -jar `which MarkDuplicates.jar` INPUT=$(RANDOM_SAMPLE_BAM).bed.sorted.bam OUTPUT=$(TMPDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.dup \
+	  METRICS_FILE=$(OUTDIR)/$(SAMPLE_NAME).Rboth.rand.uniques.sorted.spotdups.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=SILENT
