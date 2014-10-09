0cf790d5f232db779beaf7110260c555135fd512
Audra Johnson <audrakj@uw.edu>
2014-10-09 13:50:43 -0700

    All key-value pairs should be tab delineated

--- scripts/bwa/tagcounts.bash
+++ scripts/bwa/tagcounts.bash
@@ -13,14 +13,14 @@ python $STAMPIPES/scripts/bwa/bamcounts.py $INBAM $OUTPUT
 # TODO: Can probably achieve TOTAL/PF/QC in a single pass of awk
 echo "Calculating total count"
 TOTAL=`zcat ${SAMPLE_NAME}_R?_???.fastq.gz | wc -l | awk '{print $1 / 4; }'`
-echo "total $TOTAL" >> $OUTPUT
+echo -e "total\t$TOTAL" >> $OUTPUT
 
 echo "Calculating pf count"
 PF=`zcat ${SAMPLE_NAME}_R?_???.fastq.gz | \
   awk '{if (substr($2, 3, 1) == "N") {f=0;print $1} else if (substr($2, 3, 1) == "Y") {f=1} else if ( f == 0) {print $1 } }' | \
   wc -l | awk '{print $1 / 4; }'`
-echo "pf    $PF" >> $OUTPUT
+echo -e "pf\t$PF" >> $OUTPUT
 
 echo "Calculating qc count"
 QC=`awk -v TOTAL=$TOTAL -v PF=$PF 'BEGIN{ print TOTAL - PF; }'`
-echo "qc    $QC" >> $OUTPUT
+echo -e "qc\t$QC" >> $OUTPUT
