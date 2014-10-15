a7f62f4b6108c4d51605f2449c5f9920a1d9e6a8
Audra Johnson <audrakj@uw.edu>
2014-10-15 13:23:38 -0700

    Align bamcounts.py script with new filtering rules

--- scripts/bwa/bamcounts.py
+++ scripts/bwa/bamcounts.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+"""
+Filters a BWA aligned BAM file to only include uniquely mapping, properly
+paired reads.
+
+Useful SAM flag reference: http://broadinstitute.github.io/picard/explain-flags.html
+"""
+
 import os, sys, logging, re
 from pysam import Samfile
 from optparse import OptionParser
@@ -38,9 +45,6 @@ class BAMFilter(object):
 
     def process_read(self, read, counts, chrcounts, inbam):
 
-        # change this to false if we run into a condition that excludes this read
-        output = True
-
         tags = dict(read.tags)
 
         # This might not be the most perfect indicator, but it will do for now
@@ -54,102 +58,89 @@ class BAMFilter(object):
 
         if not read.tags:
             return
-        
-        if read.flag & 12:
+
+        # do not use reads with
+        # 0x4 = read is unmapped
+        # 0x8 = pair is unmapped
+        if read.flag | 12:
             return False
 
         if read.is_unmapped:
             counts['nm'] += 1
             return False
-            
+
+        # only use reads with
+        # 0x1 read paired
+        # 0x2 read mapped in proper pair
+        if not read.flag & 3:
+            return False
+
         counts['u'] += 1
-        
+
         if read.is_qcfail:
             return False
-        
+
         counts['u-pf'] += 1
-        
+
         if "N" in read.seq:
             return False
         else:
             counts['u-pf-n'] += 1
-        
-        ## 8 = mismatch, sums up all mismatch bases
-        #mismatches = sum([mismatch[1] for mismatch in read.cigar if mismatch[0] == 8])
-        #
-        #if mismatches > self.max_mismatches:
-        #    return False
-        #else:
-        #    counts['u-pf-n-mm%d' % self.max_mismatches] += 1
-        
+
         if tags['NM'] > self.max_mismatches:
             return False
         else:
             counts['u-pf-n-mm%d' % self.max_mismatches] += 1
-        
+
         chr = inbam.getrname(read.rname)
-        
+
         if not chr in chrcounts:
             chrcounts[chr] = 1
         else:
-            chrcounts[chr] += 1 
-        
+            chrcounts[chr] += 1
+
         if not "chrM" == chr:
             counts['u-pf-n-mm%d-mito' % self.max_mismatches] += 1
-        
+
         self.previous_read = read
 
-        return output
+        return True
 
     def filter(self, infile, countfile):
 
         inbam = Samfile(infile, 'rb')
-        #outbam = Samfile(outfile, 'wb', template=inbam)
-        
+
         count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
-          'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm' ]#'qc', 'mm', 'nm', 'pf', 'total']
-        
+          'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm' ]
+
         counts = dict([(label, 0) for label in count_labels])
 
         chrcounts = {}
 
         for read in inbam:
             self.process_read(read, counts, chrcounts, inbam)
-            #if output:
-            #    outbam.write(read)
-        
+
         countout = open(countfile, 'w')
-        
-        # checks
-        
-        #if counts['pf'] + counts['qc'] != counts['total']:
-        #    print "WARNING: pf + qc != total"
-        #
-        #if counts['mm'] + counts['nm'] + counts['u'] != counts['total']:
-        #    print "WARNING: mm + nm + u != total"
-        #
-        #if sum(chrcounts.values()) != counts['u-pf-n-mm%d' % self.max_mismatches]:
-        #    print "WARNING: chromosome counts != u-pf-n-mm%d" % self.max_mismatches
-        
+
         for count in count_labels:
             countout.write("%s\t%d\n" % (count, counts[count]))
-        
+
         for count in sorted(chrcounts.keys()):
             countout.write("%s\t%d\n" % (count, chrcounts[count]))
-        
+
         countout.close()
-        
+
 def main(args = sys.argv):
     """This is the main body of the program that by default uses the arguments
 from the command line."""
-    
+
     parser = parser_setup()
     (poptions, pargs) = parser.parse_args()
-    
+
     if poptions.quiet:
         logging.basicConfig(level=logging.WARNING, format=log_format)
     elif poptions.debug:
-        logging.basicConfig(level=logging.DEBUG, format=log_format)    
+        logging.basicConfig(level=logging.DEBUG, format=log_format)
     else:
         # Set up the logging levels
         logging.basicConfig(level=logging.INFO, format=log_format)
@@ -157,11 +148,11 @@ from the command line."""
     bamfile = pargs[0]
     # outfile = pargs[1]
     countfile = pargs[1]
-    
+
     filter = BAMFilter()
-    
+
     filter.filter(bamfile, countfile)
-    
+
 # This is the main body of the program that only runs when running this script
 # doesn't run when imported, so you can use the functions above in the shell after importing
 # without automatically running it
