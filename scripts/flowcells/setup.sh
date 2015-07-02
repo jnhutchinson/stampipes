75826afce9ca5d556a2ff65d1e57d959042e94ab
Jemma Nelson <nelsonjs@uw.edu>
2015-07-02 11:11:20 -0700

    Adjust setup.sh for new python3 changes
    * Source python3 activation
    * Attach flowcell directory to flowcellrun object
    * Create collation & fastq scripts
    * Create alignment scripts in new way
    * Submit all scripts (alignment scripts wait for collation)
    
    
    Former-commit-id: ebc92d65478cb6d3a1514383214a8fc8e682e6bb

--- scripts/flowcells/setup.sh
+++ scripts/flowcells/setup.sh
@@ -74,7 +74,7 @@ echo "FCID,Lane,SampleID,SampleRef,Index,Description,Control,Recipe,Operator,Sam
 
 # ( X | tostring) syntax is for non-string fields
 # Default values (if field is false or null) come after //
-jq -r --arg flowcell "$flowcell" ' 
+jq -r --arg flowcell "$flowcell" '
 .libraries as $l
 | $l
 | map( select(.failed == false) )
@@ -226,6 +226,14 @@ if [ -n "$demux" ] ; then
 fi
 
 
+flowcell_id=$( curl \
+  "$LIMS_API_URL"/flowcell_run/?label=$flowcell \
+  -H "Authorization: Token $LIMS_API_TOKEN" \
+  -k \
+  2>/dev/null \
+  | jq '.results[] | .id'
+)
+
 # The final script is below:
 cat > run_bcl2fastq.sh <<__BCL2FASTQ__
 #!/bin/bash
@@ -233,6 +241,16 @@ cat > run_bcl2fastq.sh <<__BCL2FASTQ__
 source $MODULELOAD
 module load bcl2fastq/1.8.4
 module load bcl2fastq2/2.15.0.4
+source $PYTHON3_ACTIVATE
+
+
+# Register the file directory
+python /home/audrakj/stampipes/scripts/lims/upload_data.py \
+  --attach_directory "$analysis_dir" \
+  --attach_file_contenttype sequencingdata.flowcellrun \
+  --attach_file_purpose flowcell-directory \
+  --attach_file_objectid $flowcell_id
+
 
 while [ ! -e "$illumina_dir/RTAComplete.txt" ] ; do sleep 60 ; done
 
@@ -270,13 +288,35 @@ rsync -avP "$samplesheet" "$analysis_dir"
 
 cd "$analysis_dir"
 
-python3 "$STAMPIPES/scripts/lims/get_processing.py" -f "$flowcell"
-
 $link_command
 
-python3 "$STAMPIPES/scripts/create_processing.py" --add_flowcell_scripts --ignore_failed_lanes
 
-bash run.bash
+# Create fastqc scripts
+python /home/audrakj/stampipes/scripts/laneprocess.py \
+  --script_template "$STAMPIPES/processes/fastq/fastqc.bash" \
+  --qsub-prefix .fq \
+  --sample-script-basename fastqc.bash \
+  --flowcell_label "$flowcell" \
+  --outfile fastqc.bash
+
+# Create collation scripts
+python /home/audrakj/stampipes/scripts/laneprocess.py \
+  --script_template "$STAMPIPES/processes/fastq/collate_fastq.bash" \
+  --qsub-prefix .cl \
+  --sample-script-basename "collate.bash" \
+  --flowcell_label "$flowcell" \
+  --outfile collate.bash
+
+# Create alignment scripts
+python $STAMPIPES/scripts/alignprocess.py \
+  --flowcell $flowcell \
+  --outfile run.bash
+
+bash collate.bash
+bash fastqc.bash
+
+qsub -hold_jid '.cl*' -cwd -V -q all.q -S /bin/bash run.bash
+
 __COPY__
 
 __BCL2FASTQ__
