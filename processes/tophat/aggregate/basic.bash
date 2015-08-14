6fae6dd8bbf64299f0244ce7767651612150fec8
Jemma Nelson <nelsonjs@uw.edu>
2015-08-13 17:48:37 -0700

    Working draft of basic RNA aggregation
    Former-commit-id: 61a8f73cd76277367ce261245d35e72f30326a89

---
+++ processes/tophat/aggregate/basic.bash
@@ -0,0 +1,121 @@
+
+source $MODULELOAD
+module load bedops/2.4.2
+module load java/jdk1.7.0_05
+module load gcc/4.7.2
+module load R/3.1.0
+module load picard/1.118
+module load samtools/1.2
+module load git/2.3.3
+module load coreutils/8.9
+
+cd $AGGREGATION_FOLDER
+
+export LIBRARY_NAME=LN${LIBRARY}
+
+BAM_COUNT=`ls $BAM_FILES | wc -l`
+
+JOB_BASENAME=".AGG#${AGGREGATION_ID}"
+
+export FINAL_BAM=${LIBRARY_NAME}.all.bam
+export TEMP_UNIQUES_BAM=${LIBRARY_NAME}.${GENOME}.uniques.sorted.bam
+
+if [ -n "$REDO_AGGREGATION" ]; then
+  # TODO: Make
+  bash $STAMPIPES/scripts/tophat/aggregate/basic/reset.bash
+fi
+
+MERGE_DUP_JOBNAME=${JOB_BASENAME}_merge_dup
+COUNT_JOBNAME=${JOB_BASENAME}_count
+MAKE_JOBNAME=${JOB_BASENAME}_make
+
+# Check out files match first
+python3 $STAMPIPES/scripts/utility/md5check.py bamfiles.txt || exit 1
+
+PROCESSING=""
+
+if [ ! -e ${FINAL_BAM} ]; then
+
+  PROCESSING="$PROCESSING,${MERGE_DUP_JOBNAME}"
+
+  qsub ${SUBMIT_SLOTS} -N "${MERGE_DUP_JOBNAME}" -V -cwd -S /bin/bash > /dev/stderr << __MERGE__
+    set -x -e -o pipefail
+
+    TMPBAM="$TMPDIR/$LIBRARY_NAME.bam"
+
+    echo "Hostname: "
+    hostname
+
+    echo "START: "
+    date
+
+    $STAMPIPES/scripts/tophat/merge_or_copy_bam.sh \$TMPBAM  $BAM_FILES
+
+    java -Xmx24g -jar \$(which MarkDuplicates.jar) \
+      INPUT=\$TMPBAM \
+      METRICS_FILE=$LIBRARY_NAME.spotdups.txt \
+      OUTPUT=$FINAL_BAM \
+      REMOVE_DUPLICATES=false \
+      ASSUME_SORTED=true
+
+    echo "FINISH: "
+    date
+
+__MERGE__
+
+fi
+
+make_strand(){
+  name=$1
+  targets="$name.bam $name.starch $name.bw $name.bam.bai"
+  makefile="$STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk"
+
+  if ! make -q -f $makefile $targets ; then
+    PROCESSING="$PROCESSING,${MAKE_JOBNAME}-$name"
+    qsub -N "${MAKE_JOBNAME}-$name" -hold_jid ${MERGE_DUP_JOBNAME} -V -cwd -S /bin/bash > /dev/stderr << __MAKE__
+      set -x -e -o pipefail
+
+      echo "Hostname: "
+      hostname
+
+      echo "START: "
+      date
+
+      make \
+        -f $STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk \
+        $targets
+
+      echo "FINISH: "
+      date
+
+__MAKE__
+  fi
+}
+
+make_strand $LIBRARY_NAME.all
+make_strand $LIBRARY_NAME.pos
+make_strand $LIBRARY_NAME.neg
+
+# TODO
+if [ -n ${PROCESSING} ]; then
+
+  qsub -N "${JOB_BASENAME}_complete" -hold_jid "${PROCESSING}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+  #set -x -e -o pipefail
+
+  #echo "Hostname: "
+  #hostname
+
+  #echo "START: "
+  #date
+
+  #bash $STAMPIPES/scripts/tophat/aggregate/basic/checkcomplete.bash
+  #bash $STAMPIPES/scripts/tophat/aggregate/basic/attachfiles.bash
+
+  ##rm ${TEMP_UNIQUES_BAM} ${TEMP_UNIQUES_BAM}.bai
+
+  #echo "FINISH: "
+  #date
+
+__SCRIPT__
+
+fi
