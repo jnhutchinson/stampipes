09f04e927c853578dd08c57728f944c934a9e69b
Audra Johnson <audrakj@uw.edu>
2015-08-28 11:48:10 -0700

    Single ended processing

--- processes/bwa/process_bwa_unpaired.bash
+++ processes/bwa/process_bwa_unpaired.bash
@@ -23,12 +23,6 @@ MAX_MISMATCHES=2
 MIN_MAPPING_QUALITY=10
 MAX_INSERT_SIZE=750
 
-export BASE_PRIORITY=${BASE_PRIORITY:-"-10"}
-
-if [ -n $ALN_PRIORITY ]; then
-  export ALN_PRIORITY=`echo "$BASE_PRIORITY - 10" | bc`
-fi
-
 JOB_BASENAME=${SAMPLE_NAME}_${FLOWCELL}_ALIGN#${ALIGNMENT_ID}
 
 export FINAL_BAM=${SAMPLE_NAME}.sorted.bam
@@ -62,7 +56,7 @@ python3 $STAMPIPES/scripts/lims/upload_data.py -a ${LIMS_API_URL} \
   --version_file $VERSION_FILE
 
 if [ ! -e "$FASTQ_TMP/${SAMPLE_NAME}_R1_000.fastq.gz" ]; then
-  bash $STAMPIPES/scripts/fastq/splitfastq.bash $FASTQ_TMP $R1_FASTQ $R2_FASTQ 
+  bash $STAMPIPES/scripts/fastq/splitfastq.bash $FASTQ_TMP $R1_FASTQ $R2_FASTQ
 fi
 
 fi
@@ -78,7 +72,7 @@ do
     
 if [ ! -e $BAMFILE -a ! -e ${FINAL_BAM} ]; then
 
-qsub -p $ALN_PRIORITY -l h_data=5650M -N ${NAME} -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub -l h_data=5650M -N ${NAME} -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
 
   echo "Hostname: "
@@ -87,23 +81,9 @@ qsub -p $ALN_PRIORITY -l h_data=5650M -N ${NAME} -V -cwd -S /bin/bash > /dev/std
   echo "START: "
   date
 
-  if [ "$UMI" = "True" ]; then
-
-  make -f $STAMPIPES/makefiles/bwa/bwa_paired_trimmed_umi.mk \
-    FASTQ1_FILE=${FASTQ_TMP}/${SAMPLE_NAME}_R1_${filenum}.fastq.gz \
-    FASTQ2_FILE=${FASTQ_TMP}/${SAMPLE_NAME}_R2_${filenum}.fastq.gz \
-    OUTBAM=${BAMFILE} \
-    TRIMSTATS=${SAMPLE_NAME}_${filenum}.trimstats.txt
-
-  else
-
-  make -f $STAMPIPES/makefiles/bwa/bwa_paired_trimmed.mk \
-    FASTQ1_FILE=${FASTQ_TMP}/${SAMPLE_NAME}_R1_${filenum}.fastq.gz \
-    FASTQ2_FILE=${FASTQ_TMP}/${SAMPLE_NAME}_R2_${filenum}.fastq.gz \
-    OUTBAM=${BAMFILE} \
-    TRIMSTATS=${SAMPLE_NAME}_${filenum}.trimstats.txt
-
-  fi
+  make -f $STAMPIPES/makefiles/bwa/bwa_single.mk \
+    FASTQ_FILE=${FASTQ_TMP}/${SAMPLE_NAME}_R1_${filenum}.fastq.gz \
+    OUTBAM=${BAMFILE}
 
   echo "FINISH: " `date`
 
@@ -119,7 +99,6 @@ fi
 FASTQ_PAIR_BAMS="${BAMFILE} ${FASTQ_PAIR_BAMS}"
 
 done
-
 
 if [ -n "$FASTQ_PAIR_HOLDS" ]; then
     SPLIT_ALIGN_HOLD="-hold_jid $FASTQ_PAIR_HOLDS"
@@ -137,16 +116,7 @@ PROCESS_HOLD="-hold_jid .pb${JOB_BASENAME}"
 JOBNAME=".pb${JOB_BASENAME}"
 PROCESSING="$PROCESSING,$JOBNAME"
 
-# If we are processing UMI, we will need a lot of power for sorting!
-if [ "$UMI" = "True" ]; then
-  export SUBMIT_SLOTS="-pe threads 2"
-fi
-
-export PRE_DUP_BAM=${SAMPLE_NAME}.predup.sorted.bam
-export PRE_DUP_BAM_PREFIX=${PRE_DUP_BAM%.*}
-export FINAL_BAM_PREFIX=${FINAL_BAM%.*}
-
-qsub -p $BASE_PRIORITY ${SPLIT_ALIGN_HOLD} ${SUBMIT_SLOTS} -N "$JOBNAME" -V -cwd -S /bin/bash << __SCRIPT__
+qsub ${SPLIT_ALIGN_HOLD} ${SUBMIT_SLOTS} -N "$JOBNAME" -V -cwd -S /bin/bash << __SCRIPT__
   set -x -e -o pipefail
 
   echo "Hostname: "
@@ -165,23 +135,6 @@ qsub -p $BASE_PRIORITY ${SPLIT_ALIGN_HOLD} ${SUBMIT_SLOTS} -N "$JOBNAME" -V -cwd
       samtools merge ${FINAL_BAM} ${FASTQ_PAIR_BAMS}
     fi
   
-    # If we are working with a UMI, we can create the duplicate score and replace
-    # the final BAM with one marked with duplicates
-    if [ "$UMI" = "True" ]; then
-      echo "START UMI DUP: "
-      date
-  
-      rsync $FINAL_BAM \$TMPDIR/${PRE_DUP_BAM}
-      rm $FINAL_BAM
-  
-      make -f $STAMPIPES/makefiles/umi/mark_duplicates.mk INPUT_BAM_FILE=\$TMPDIR/${PRE_DUP_BAM} \
-        OUTPUT_BAM_FILE=\$TMPDIR/${FINAL_BAM}.presort
-      samtools sort \$TMPDIR/${FINAL_BAM}.presort $FINAL_BAM_PREFIX
-  
-      echo "FINISH UMI DUP: "
-      date
-    fi
-
     echo "FINISH MERGE: "
     date
   else
@@ -191,7 +144,7 @@ qsub -p $BASE_PRIORITY ${SPLIT_ALIGN_HOLD} ${SUBMIT_SLOTS} -N "$JOBNAME" -V -cwd
   echo "START PROCESS BAM: "
   date
 
-  make -f $STAMPIPES/makefiles/bwa/process_paired_bam.mk
+  make -f $STAMPIPES/makefiles/bwa/process_unpaired_bam.mk
 
   echo "FINISH PROCESS BAM: "
   date
@@ -208,7 +161,6 @@ qsub -p $BASE_PRIORITY ${SPLIT_ALIGN_HOLD} ${SUBMIT_SLOTS} -N "$JOBNAME" -V -cwd
     -f ${FLOWCELL} \
     --alignment_id ${ALIGNMENT_ID} \
     --flowcell_lane_id ${FLOWCELL_LANE_ID} \
-    --insertsfile ${SAMPLE_NAME}.CollectInsertSizeMetrics.picard \
     --dupsfile ${SAMPLE_NAME}.MarkDuplicates.picard
   
   if [ "$NUMBER_FASTQ_FILES" -gt "1" ]
@@ -229,7 +181,7 @@ if [ ! -e ${SAMPLE_NAME}.tagcounts.txt -o -n "$FORCE_COUNTS" ]; then
 JOBNAME=".ct${JOB_BASENAME}"
 PROCESSING="$PROCESSING,$JOBNAME"
    
-qsub -p $BASE_PRIORITY $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
   echo "Hostname: "
   hostname
@@ -237,7 +189,7 @@ qsub -p $BASE_PRIORITY $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/s
   echo "START: " 
   date
 
-  time bash $STAMPIPES/scripts/bwa/tagcounts.bash $SAMPLE_NAME $SAMPLE_NAME.sorted.bam $SAMPLE_NAME.tagcounts.txt $R1_FASTQ $R2_FASTQ
+  time bash $STAMPIPES/scripts/bwa/tagcounts.bash $SAMPLE_NAME $SAMPLE_NAME.sorted.bam $SAMPLE_NAME.tagcounts.txt $FASTQ_DIR
   # upload all data to the LIMS
   python3 $STAMPIPES/scripts/lims/upload_data.py -a ${LIMS_API_URL} \
       -t ${LIMS_API_TOKEN} \
@@ -258,7 +210,7 @@ if [ ! -e ${SAMPLE_NAME}.R1.rand.uniques.sorted.spot.out -o ! -e ${SAMPLE_NAME}.
 JOBNAME=".sp${JOB_BASENAME}"
 PROCESSING="$PROCESSING,$JOBNAME" 
     
-qsub -p $BASE_PRIORITY $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
 
   echo "Hostname: "
@@ -267,7 +219,7 @@ qsub -p $BASE_PRIORITY $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/s
   echo "START: "
   date
 
-  make -f $STAMPIPES/makefiles/SPOT/spot-R1-paired.mk BWAINDEX=$BWAINDEX ASSAY=$ASSAY GENOME=$GENOME \
+  make -f $STAMPIPES/makefiles/SPOT/spot-R1.mk BWAINDEX=$BWAINDEX ASSAY=$ASSAY GENOME=$GENOME \
     READLENGTH=$READLENGTH SAMPLE_NAME=$SAMPLE_NAME
   # upload all data to the LIMS
   python3 $STAMPIPES/scripts/lims/upload_data.py -a ${LIMS_API_URL} \
@@ -290,7 +242,7 @@ if [ ! -e ${SAMPLE_NAME}.75_20.${GENOME}.bw ]; then
 JOBNAME=".den${JOB_BASENAME}"
 PROCESSING="$PROCESSING,$JOBNAME" 
 
-qsub -p $BASE_PRIORITY $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub $PROCESS_HOLD -N "$JOBNAME" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
 
   echo "Hostname: "
@@ -311,7 +263,7 @@ fi
 
 if [ -n "$PROCESSING" ]; then
 
-qsub -p $BASE_PRIORITY -hold_jid ${PROCESSING} -N ".com${JOB_BASENAME}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+qsub -hold_jid ${PROCESSING} -N ".com${JOB_BASENAME}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   set -x -e -o pipefail
   echo "Hostname: "
   hostname
--- scripts/bwa/bamcounts.py
+++ scripts/bwa/bamcounts.py
@@ -16,6 +16,7 @@ log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 script_options = {
   "debug": False,
   "quiet": True,
+  "paired": True,
 }
 
 def parser_setup():
@@ -36,6 +37,9 @@ def parser_setup():
     parser.add_argument("--max_mismatches", dest="max_mismatches", type=int, default=2,
         help="Maximum mismatches for filtering")
 
+    parser.add_argument("--unpaired", dest="paired", action="store_false",
+        help="Count style is unpaired.")
+
     parser.set_defaults( **script_options )
     parser.set_defaults( quiet=False, debug=False )
 
@@ -43,15 +47,19 @@ def parser_setup():
 
 class BAMFilter(object):
 
-    def __init__(self, max_mismatches=2, min_mapping_quality=10):
+    def __init__(self, max_mismatches=2, min_mapping_quality=10, paired=True):
 
         self.max_mismatches = max_mismatches
         self.previous_read = None
         self.min_mapping_quality = min_mapping_quality
+        self.paired = paired
+        self.upfnmm = 'u-pf-n-mm%d' % self.max_mismatches
+        self.upfnmmmito = 'u-pf-n-mm%d-mito' % self.max_mismatches
 
-    def process_read(self, read, inbam): 
-
-        tags = dict(read.tags)
+    def process_flags(self, read, inbam):
+        """
+        Do a tally of mapq values, samflag values, and read lengths
+        """
 
         mapq = "mapq-%d" % read.mapq
         if not mapq in self.mapqcounts:
@@ -70,6 +78,43 @@ class BAMFilter(object):
             self.readlengthcounts[readlength] = 1
         else:
             self.readlengthcounts[readlength] += 1
+
+    def process_read_paired(self, read, inbam):
+        """
+        Check a read for paired alignment conditions, return False if they are not met
+        """
+
+        # do not use reads with
+        # 0x4 = read is unmapped
+        # 0x8 = pair is unmapped
+        if read.flag & 12:
+            return False
+
+        # only use reads with
+        # 0x1 read paired
+        # 0x2 read mapped in proper pair
+        if not (read.flag & 1 and read.flag & 2):
+            return False
+
+        # Figure out how many alignments aren't included because of mapq
+        if self.min_mapping_quality > read.mapq:
+            self.counts['paired-mapq-filter'] += 1
+            return False
+
+        # do not use reads with QC fail even if they pass all other checks
+        # 0x512 QC Fail
+        if read.flag & 512:
+            self.counts['paired-aligned-qcfail'] += 1
+            return False
+
+        self.counts['paired-aligned'] += 1
+
+        return True
+
+    def process_read(self, read, inbam):
+
+        tags = dict(read.tags)
+        self.process_flags(read, inbam)
 
         # This might not be the most perfect indicator, but it will do for now
         # Must take place before minimum quality filter--most multiple matching
@@ -91,44 +136,18 @@ class BAMFilter(object):
             self.counts['all-mapq-filter'] += 1
             return
 
-        # do not use reads with
-        # 0x4 = read is unmapped
-        # 0x8 = pair is unmapped
-        if read.flag & 12:
-            return False
-
-        # only use reads with
-        # 0x1 read paired
-        # 0x2 read mapped in proper pair
-        if not (read.flag & 1 and read.flag & 2):
-            return False
-
-        # Figure out how many alignments aren't included because of mapq
-        if self.min_mapping_quality > read.mapq:
-            self.counts['paired-mapq-filter'] += 1
-            return
-
-        # do not use reads with QC fail even if they pass all other checks
-        # 0x512 QC Fail
-        if read.flag & 512:
-            self.counts['paired-aligned-qcfail'] += 1
-            return
-
         chr = inbam.getrname(read.rname)
         nuclear = not chr in ("chrM", "chrC")
 
-        if nuclear:
-            self.counts['paired-nuclear-align'] += 1
-
-        if nuclear and not chr in ("chrX", "chrY", "chrZ", "chrW"):
-            self.counts['paired-autosomal-align'] += 1
+        # Do our paired alignment checks, return if we don't pass here
+        if self.paired and not self.process_read_paired(read, inbam):
+            return
 
         if read.flag & 1024:
             self.counts['umi-duplicate'] += 1
             if nuclear:
                 self.counts['umi-duplicate-nuclear'] += 1
 
-        self.counts['paired-aligned'] += 1
         self.counts['u'] += 1
 
         passreadlength = "aligned-readlength-%d" % read.rlen
@@ -152,16 +171,26 @@ class BAMFilter(object):
         else:
             self.counts['u-pf-n-mm%d' % self.max_mismatches] += 1
 
-
         if not chr in self.chrcounts:
             self.chrcounts[chr] = 1
         else:
             self.chrcounts[chr] += 1
 
-        if not "chrM" == chr:
-            self.counts['u-pf-n-mm%d-mito' % self.max_mismatches] += 1
+        if self.paired:
+            nuclear_align_count = "paired-nuclear-align"
+            autosomal_align_count = "paired-autosomal-align"
+        else:
+            nuclear_align_count = "nuclear-align"
+            autosomal_align_count = "autosomal-align"
 
-        self.previous_read = read
+        if nuclear:
+            self.counts[nuclear_align_count] += 1
+
+        if nuclear and not chr in ("chrX", "chrY", "chrZ", "chrW"):
+            self.counts[autosomal_align_count] += 1
+
+        if self.paired:
+            self.counts['paired-aligned'] += 1
 
         return True
 
@@ -174,10 +203,61 @@ class BAMFilter(object):
 
         inbam = Samfile(infile, 'rb')
 
-        count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
-          'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm', 'nm', 'qc-flagged', 'umi-duplicate', 'umi-duplicate-nuclear',
-          'all-aligned', 'paired-aligned', 'paired-nuclear-align', 'paired-autosomal-align', 
-          'all-mapq-filter', 'paired-mapq-filter', 'paired-aligned-qcfail']
+        if self.paired:
+            logging.debug("Processing paired reads")
+            count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
+              'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm', 'nm', 'qc-flagged', 'umi-duplicate', 'umi-duplicate-nuclear',
+              'all-aligned', 'paired-aligned', 'nuclear-align', 'autosomal-align', 
+              'all-mapq-filter', 'paired-mapq-filter', 'paired-aligned-qcfail']
+        else:
+            logging.debug("Processing unpaired reads")
+            count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
+               'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm', 'nm', 'qc-flagged', 'umi-duplicate', 'umi-duplicate-nuclear',
+               'all-aligned', 'all-mapq-filter', 'nuclear-align', 'autosomal-align']
+
+        logging.debug(count_labels)
+        self.counts = dict([(label, 0) for label in count_labels])
+
+        self.chrcounts = {}
+        self.mapqcounts = {}
+        self.samflagcounts = {}
+        self.readlengthcounts = {}
+
+        for read in inbam:
+            self.process_read(read, inbam)
+
+        countout = open(countfile, 'a')
+
+        self.write_dict(countout, self.counts)
+        self.write_dict(countout, self.chrcounts)
+        self.write_dict(countout, self.mapqcounts)
+        self.write_dict(countout, self.samflagcounts)
+        self.write_dict(countout, self.readlengthcounts)
+
+        countout.close()
+
+    def write_dict(self, countout, counts):
+
+        for count in sorted(counts.keys()):
+            countout.write("%s\t%d\n" % (count, counts[count]))
+
+    def filter(self, infile, countfile):
+
+        inbam = Samfile(infile, 'rb')
+
+        if self.paired:
+            logging.debug("Processing paired reads")
+            count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
+              'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm', 'nm', 'qc-flagged', 'umi-duplicate', 'umi-duplicate-nuclear',
+              'all-aligned', 'paired-aligned', 'paired-nuclear-align', 'paired-autosomal-align', 
+              'all-mapq-filter', 'paired-mapq-filter', 'paired-aligned-qcfail']
+        else:
+            logging.debug("Processing unpaired reads")
+            count_labels = ['u', 'u-pf', 'u-pf-n', 'u-pf-n-mm%d' % self.max_mismatches,
+               'u-pf-n-mm%d-mito' % self.max_mismatches, 'mm', 'nm', 'qc-flagged', 'umi-duplicate', 'umi-duplicate-nuclear',
+               'all-aligned', 'all-mapq-filter', 'nuclear-align', 'autosomal-align']
+
+        logging.debug(count_labels)
 
         self.counts = dict([(label, 0) for label in count_labels])
 
@@ -217,7 +297,7 @@ from the command line."""
     bamfile = args.bamfile
     countfile = args.outfile
 
-    filter = BAMFilter(max_mismatches=args.max_mismatches, min_mapping_quality=args.min_mapping_quality)
+    filter = BAMFilter(max_mismatches=args.max_mismatches, min_mapping_quality=args.min_mapping_quality, paired = args.paired)
     filter.filter(bamfile, countfile)
 
 # This is the main body of the program that only runs when running this script
