c30ba13c6f0d52d62d84bb28dda64d69d1468ea1
Audra Johnson <audrakj@uw.edu>
2015-10-02 19:11:12 -0700

    Auto delete collated files
    * Detects existing files and retains them unless told not to
    * Delete originals unless told not to
    * Detects if we can't go forward due to lack of original files

--- processes/fastq/collate_fastq.bash
+++ processes/fastq/collate_fastq.bash
@@ -1,84 +1,120 @@
+# If RETAIN_ORIGINALS is set, then do not delete files we are collating from
+# If REDO_COLLATION is set, then do collation again; this will only work if originals still exist
+
+# Ensure that script failures have the script quit before it deletes files
+set -e 
 
 cd $FASTQ_DIR
 
 FASTQ_NAME=${FLOWCELL}_${SAMPLE_NAME}
 
-R1_NUM_FILES=$(ls ${SAMPLE_NAME}_R1_???.fastq.gz | wc -l)
+R1_NUM_FILES=$(find . -name "${SAMPLE_NAME}_R1_???.fastq.gz" | wc -l)
 
 if [[ "$PAIRED" == "True" ]]; then
 
-  R2_NUM_FILES=$(ls ${SAMPLE_NAME}_R2_???.fastq.gz | wc -l)
+  R2_NUM_FILES=$(find . -name "${SAMPLE_NAME}_R2_???.fastq.gz" | wc -l)
 
   if [[ "$R1_NUM_FILES" -ne "$R2_NUM_FILES" ]]; then
-
     echo "UNEQUAL NUMBER OF FILES FOR $SAMPLE_NAME IN $FASTQ_DIR"
     exit 1
   fi
 fi
 
-UPLOAD_SCRIPT="python3 $STAMPIPES/scripts/lims/upload_data.py --attach_file_contenttype SequencingData.flowcelllane --attach_file_objectid ${FLOWCELL_LANE_ID} --attach_file_type=gzipped-fastq"
-
 R1_FILE=${FASTQ_NAME}_R1.fastq.gz
 R2_FILE=${FASTQ_NAME}_R2.fastq.gz
 
-R1_TMP_FILE=$(mktemp)
-R2_TMP_FILE=$(mktemp)
+function upload {
+  UPLOAD_SCRIPT="python3 $STAMPIPES/scripts/lims/upload_data.py --attach_file_contenttype SequencingData.flowcelllane --attach_file_objectid ${FLOWCELL_LANE_ID} --attach_file_type=gzipped-fastq"
+  $UPLOAD_SCRIPT --attach_file_purpose r1-fastq --attach_file ${R1_FILE}
 
-if [ -e "$R1_FILE" ]; then
-  rm "$R1_FILE"
+  if [ -e $R2_FILE ]; then
+    $UPLOAD_SCRIPT --attach_file_purpose r2-fastq --attach_file ${R2_FILE}
+  fi
+}
+
+if [ -e "$R1_FILE" -a ! -n "$REDO_COLLATION" ]; then
+  echo "Collated files already exist, syncing with LIMS"
+  upload
+  exit
 fi
 
-if [ -e "$R2_FILE" ]; then
-  rm "$R2_FILE"
+if [ -n "$REDO_COLLATION" -a "$R1_NUM_FILES" -eq 0 ]; then
+  echo "Cannot redo collation, no original files"
+  upload
+  exit
 fi
 
-echo "R1: $R1_FILE"
-echo "R2: $R2_FILE"
+if [ "$R1_NUM_FILES" -ge "1" ]; then
 
-for filenum in $(seq -f "%03g" 1 $R1_NUM_FILES)
-do
+  R1_TMP_FILE=$(mktemp)
+  R2_TMP_FILE=$(mktemp)
 
-  echo "Adding ${filenum} to collated files"
-
-  cat ${SAMPLE_NAME}_R1_${filenum}.fastq.gz >> $R1_TMP_FILE
-
-  if [[ "$PAIRED" == "True" ]]; then
-    R2_FILE=${FASTQ_NAME}_R2.fastq.gz
-    cat ${SAMPLE_NAME}_R2_${filenum}.fastq.gz  >> $R2_TMP_FILE
+  if [ -e "$R1_FILE" ]; then
+    rm "$R1_FILE"
   fi
 
-done
+  if [ -e "$R2_FILE" ]; then
+    rm "$R2_FILE"
+  fi
 
-gzip -t $R1_TMP_FILE
+  echo "R1: $R1_FILE"
+  echo "R2: $R2_FILE"
 
-if [ ! -s $R1_TMP_FILE ]; then
-  echo "ERROR: $R1_TMP_FILE is 0 size"
+  for filenum in $(seq -f "%03g" 1 $R1_NUM_FILES)
+  do
+
+    echo "Adding ${filenum} to collated files"
+
+    cat ${SAMPLE_NAME}_R1_${filenum}.fastq.gz >> $R1_TMP_FILE
+
+    if [[ "$PAIRED" == "True" ]]; then
+      R2_FILE=${FASTQ_NAME}_R2.fastq.gz
+      cat ${SAMPLE_NAME}_R2_${filenum}.fastq.gz  >> $R2_TMP_FILE
+    fi
+
+  done
+
+  # Ensure we have valid gzipped files
+  gzip -t $R1_TMP_FILE
+
+  if [ ! -s $R1_TMP_FILE ]; then
+    echo "ERROR: $R1_TMP_FILE is 0 size"
+    exit 1
+  fi
+
+  if [ "$PAIRED" == "True" ]; then
+    gzip -t $R2_TMP_FILE
+    if [ ! -s $R2_TMP_FILE ]; then
+      echo "ERROR: $R2_TMP_FILE is 0 size"
+      exit 1
+    fi
+  fi
+
+  rsync "$R1_TMP_FILE" "$R1_FILE"
+  # Files created in temp directories do not have appropriate
+  # permissions; make sure our collated files can be read by
+  # anybody
+  chmod 644 $R1_FILE
+  rm $R1_TMP_FILE
+  if [[ "$PAIRED" == "True" ]] ; then
+    rsync "$R2_TMP_FILE" "$R2_FILE"
+    chmod 644 $R2_FILE
+    rm $R2_TMP_FILE
+  fi
+else
+  echo "No R1 files to work with"
   exit 1
 fi
 
-if [ "$PAIRED" == "True" ]; then
-  gzip -t $R2_TMP_FILE
-  if [ ! -s $R2_TMP_FILE ]; then
-    echo "ERROR: $R2_TMP_FILE is 0 size"
-    exit 1
+upload
+
+# Remove existing pre-collation files
+if [ ! -n "$RETAIN_ORIGINALS" ]; then
+  echo "Removing R1 originals"
+  rm ${SAMPLE_NAME}_R1_???.fastq.gz
+
+  if [[ "$PAIRED" == "True" ]]; then
+    echo "Removing R2 originals"
+    rm ${SAMPLE_NAME}_R2_???.fastq.gz
   fi
-fi
-
-rsync "$R1_TMP_FILE" "$R1_FILE"
-# Files created in temp directories do not have appropriate
-# permissions; make sure our collated files can be read by
-# anybody
-chmod 644 $R1_FILE
-rm $R1_TMP_FILE
-if [[ "$PAIRED" == "True" ]] ; then
-  rsync "$R2_TMP_FILE" "$R2_FILE"
-  chmod 644 $R2_FILE
-  rm $R2_TMP_FILE
-fi
-
-
-$UPLOAD_SCRIPT --attach_file_purpose r1-fastq --attach_file ${R1_FILE}
-
-if [ -e $R2_FILE ]; then
-  $UPLOAD_SCRIPT --attach_file_purpose r2-fastq --attach_file ${R2_FILE}
 fi
