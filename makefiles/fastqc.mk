607583c9c5ff088a492b637d462bb90b86fe113f
Audra Johnson <audrakj@uw.edu>
2015-10-14 17:42:38 -0700

    Fix fastqc process for R1 and R2 fastq set ups

--- makefiles/fastqc.mk
+++ makefiles/fastqc.mk
@@ -1,19 +1,9 @@
 ###################
 # These variables must be passed in or set for the makefile to work.
 ###################
-# SAMPLE_NAME=Example_NoIndex_L007
+# FASTQ_FILE=/path/to/file.fastq.gz
+# FASTQC_FILE=/path/to/file_fastqc.zip
 ###################
-# If the data is paired, then indicate this
-###################
-# PAIRED=True
-###################
-# If uploading data to the LIMS, then indicate the LIMS_API_TOKEN to use and FLOWCELL uploading name
-# and the ALIGNMENT_ID for the counts
-###################
-# LIMS_API_URL=http://lims.com
-# LIMS_API_TOKEN=aethua87ao997i9a9u
-# ALIGNMENT_ID=1435
-# FLOWCELL=CU8TE
 ###################
 
 # Use the default PATH version of each of these programs unless overridden
@@ -26,26 +16,19 @@ PYTHON ?= python3
 # well within qsub memory limits
 FASTQC_OPTIONS ?= -t 8 --noextract --nogroup
 
-INDIR ?= $(shell pwd)
+ORIGINAL_FASTQC_OUTFILE ?= $(basename $(basename $(FASTQ_FILE) ))_fastqc.zip
+
+FASTQC_FILE ?= $(ORIGINAL_FASTQC_OUTFILE)
 
 OUTDIR ?= $(shell pwd)
 
 STAMPIPES ?= ~/stampipes
 
-ifdef PAIRED
- TARGETS ?= $(OUTDIR)/$(SAMPLE_NAME)_R1_fastqc.zip $(OUTDIR)/$(SAMPLE_NAME)_R2_fastqc.zip
-else
- TARGETS ?= $(OUTDIR)/$(SAMPLE_NAME)_R1_fastqc.zip
-endif 
+.PHONY : info
 
-NUM_FILES = $(shell ls $(INDIR)/$(SAMPLE_NAME)_R1_???.fastq.gz | wc -l)
+all : info fastqc
 
-.PHONY : upload fastqc info
-
-all : info fastqc upload
-
-fastqc : $(TARGETS) 
-	@echo "Targets: " $(TARGETS)
+fastqc : $(FASTQC_FILE)
 	
 info :
 	@echo "------"
@@ -56,35 +39,17 @@ info :
 	@echo "------"
 	@echo "METADATA"
 	@echo "------"
-	@echo "SAMPLE_NAME: " $(SAMPLE_NAME)
+	@echo "FILE: " $(FASTQ_FILE)
 	@echo "------"
 	@echo "PROGRAM OPTIONS"
 	@echo "------"
 	@echo "FASTQC_OPTIONS: " $(FASTQC_OPTIONS)
 	@echo "------"
 
-# Make fastqc results
-# Also move the folder if we only have one file pair so we don't have the extra 001
-# We can't specify FastQC's output filename so if there's only one file it won't
-# assume it's a group and will leave on the 001
-$(OUTDIR)/$(SAMPLE_NAME)_%_fastqc.zip :
-	@echo FastQ files: $(NUM_FILES)
-	time $(FASTQC) $(FASTQC_OPTIONS) $(INDIR)/$(SAMPLE_NAME)_$*_???.fastq.gz --casava && echo FastQC stats >&2
-ifeq ($(NUM_FILES), 1)
-	mv $(OUTDIR)/$(SAMPLE_NAME)_$*_001_fastqc.html $(OUTDIR)/$(SAMPLE_NAME)_$*_fastqc.html
-	mv $(OUTDIR)/$(SAMPLE_NAME)_$*_001_fastqc.zip $(OUTDIR)/$(SAMPLE_NAME)_$*_fastqc.zip
-endif
-
-ifdef LIMS_API_TOKEN
-upload : $(TARGETS)
-	@echo "Uploading FastQC data"
-ifdef PAIRED
-	@echo "Paired end upload"
-	$(PYTHON) $(STAMPIPES)/scripts/lims/upload_data.py -f $(FLOWCELL) --flowcell_lane_id=$(FLOWCELL_LANE_ID) \
-	  --fastqcfile $(OUTDIR)/$(SAMPLE_NAME)_R1_fastqc.zip --fastqcfile $(OUTDIR)/$(SAMPLE_NAME)_R2_fastqc.zip --fastqc_counts
-else
-	@echo "Single end upload"
-	$(PYTHON) $(STAMPIPES)/scripts/lims/upload_data.py -f $(FLOWCELL) --flowcell_lane_id=$(FLOWCELL_LANE_ID) \
-	  --fastqcfile $(OUTDIR)/$(SAMPLE_NAME)_R1_fastqc.zip --fastqc_counts
-endif
+$(FASTQC_FILE) : $(FASTQ_FILE)
+	@echo FastQ file: $(FASTQ_FILE)
+	time $(FASTQC) -o $(OUTDIR) $(FASTQC_OPTIONS) $(FASTQ_FILE) && echo FastQC stats >&2
+ifneq ($(FASTQC_FILE),$(ORIGINAL_FASTQC_OUTFILE))
+	@echo Moving to $(FASTQC_FILE) from $(ORIGINAL_FASTQC_OUTFILE)
+	mv $(ORIGINAL_FASTQC_OUTFILE) $(FASTQC_FILE)
 endif
--- processes/fastq/fastqc.bash
+++ processes/fastq/fastqc.bash
@@ -4,9 +4,16 @@ module load java/jdk1.7.0_05
 module load picard/1.118
 module load FastQC/0.11.3
 
+export FASTQ_NAME=${FLOWCELL}_${SAMPLE_NAME}
+export R1_FASTQ=${FASTQ_NAME}_R1.fastq.gz
+export R2_FASTQ=${FASTQ_NAME}_R2.fastq.gz
+export R1_FASTQC=${FASTQ_NAME}_R1_fastqc.zip
+export R2_FASTQC=${FASTQ_NAME}_R2_fastqc.zip
+export TOP_UMIS=${SAMPLE_NAME}.topumis.txt
+
 cd $FASTQ_DIR
 
-if [ ! -e ${SAMPLE_NAME}_R1_fastqc.zip -o ! -e ${SAMPLE_NAME}_R2_fastqc.zip ]; then
+if [ ! -e "$R1_FASTQC" -o ! -e "$R2_FASTQC" ]; then
 
 PROCESS_NAME=".fq${SAMPLE_NAME}_${FLOWCELL}_LANE#${FLOWCELL_LANE_ID}"
 
@@ -20,12 +27,20 @@ qsub -N ".fq${SAMPLE_NAME}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
   date
 
   cd $FASTQ_DIR
-  make -f $STAMPIPES/makefiles/fastqc.mk
+  make -f $STAMPIPES/makefiles/fastqc.mk FASTQ_FILE=$R1_FASTQ FASTQC_FILE=$R1_FASTQC
+  make -f $STAMPIPES/makefiles/fastqc.mk FASTQ_FILE=$R2_FASTQ FASTQC_FILE=$R2_FASTQC
 
   if [ "$UMI" = "True" ]; then
       echo "Tallying up top UMI tags seen in R1"
-      zcat ${R1_FASTQ} | grep "^@" | cut -f 2 -d "+" | sort | uniq -c | sort -n -r | head -n 100 > ${SAMPLE_NAME}.topumis.txt
+      zcat ${R1_FASTQ} | grep "^@" | cut -f 2 -d "+" | sort | uniq -c | sort -n -r | head -n 100 > ${TOP_UMIS}
   fi
+
+  if [ "$PAIRED" = "True" ]; then
+      python3 ${STAMPIPES}/scripts/lims/upload_data.py -f ${FLOWCELL} --flowcell_lane_id=${FLOWCELL_LANE_ID} \
+	  --fastqcfile $R1_FASTQC --fastqcfile $R2_FASTQC
+  else
+      python3 ${STAMPIPES}/scripts/lims/upload_data.py -f ${FLOWCELL} --flowcell_lane_id=${FLOWCELL_LANE_ID} \
+	  --fastqcfile $R1_FASTQC
 
   bash $STAMPIPES/scripts/fastq/attachfiles.bash
 
