43b22fc982e329396c6c3b408bfbfae2ddd844d3
Jemma Nelson <nelsonjs@uw.edu>
2015-10-19 15:48:39 -0700

    Disable upload of FastQC raw-reads to LIMS
    Was overwriting the more helpful `r1-fastq` and `r2-fastq` file
    purposes.
    
    
    Former-commit-id: 558845523d889a38efcad4b22808a87ecf7f5769

--- scripts/fastq/attachfiles.bash
+++ scripts/fastq/attachfiles.bash
@@ -9,15 +9,12 @@
 UPLOAD_SCRIPT=$STAMPIPES/scripts/lims/upload_data.py
 
 ATTACH_LANE="python3 $UPLOAD_SCRIPT --attach_file_contenttype SequencingData.flowcelllane --attach_file_object ${FLOWCELL_LANE_ID}"
-ATTACH_FASTQ="$ATTACH_LANE --attach_file_purpose raw-reads --attach_file_type gzipped-fastq"
 
 $ATTACH_LANE --attach_directory ${FASTQ_DIR} --attach_file_purpose fastq-directory
 
 $ATTACH_LANE --attach_file ${R1_FASTQC} --attach_file_type zip --attach_file_purpose fastqc-results-zip
-$ATTACH_FASTQ --attach_file ${R1_FASTQ}
 
 if [ "$PAIRED" = "True" ]; then
     $ATTACH_LANE --attach_file ${R2_FASTQC} --attach_file_type zip --attach_file_purpose fastqc-results-zip
-    $ATTACH_FASTQ --attach_file ${R2_FASTQ}
 fi
 
