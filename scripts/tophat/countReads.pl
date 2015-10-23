d900c777ec4b0ac500c02910ab803fe61095d82c
Jemma Nelson <nelsonjs@uw.edu>
2015-10-22 20:35:47 -0700

    Improve tophat process in this brave new post-collation world
    Don't need to do multiple tophat alignments & merge the results; just do
    one. Same with trimming and other steps.

--- scripts/tophat/countReads.pl
+++ scripts/tophat/countReads.pl
@@ -4,17 +4,13 @@ use strict;
 # add up lines in fastq files, print name key value
 
 if (@ARGV < 3) {
-    die "Usage:  $0 name key [reads directory]\n";
+    die "Usage:  $0 name key <readfile> [readfiles...]\n";
 }
 
-my ($name,$key,$readsDir) = @ARGV;
-
-unless (-d $readsDir) {
-    die "($readsDir) directory not found\n";
-}
+my ($name,$key,@readFiles) = @ARGV;
 
 my $sum = 0;
-foreach my $file (glob("$readsDir/${name}_R?_???.fastq.gz")) {
+foreach my $file (@readFiles) {
     chomp( my $lines = `zcat $file | wc -l` );
     $sum += ($lines / 4);
 }
