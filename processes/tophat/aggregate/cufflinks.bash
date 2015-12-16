f33062144795864c5f156e1846ed10e184e65acd
Jemma Nelson <nelsonjs@uw.edu>
2015-12-16 12:47:45 -0800

    Add cufflinks aggregation option
    Like tophat/aggregate/basic.bash, but also runs cufflinks.
    Not yet super battle-tested, used for the R21-33 transition aggregations.

---
+++ processes/tophat/aggregate/cufflinks.bash
@@ -0,0 +1,165 @@
+source $PYTHON3_ACTIVATE
+
+source $MODULELOAD
+module load bedops/2.4.14
+module load java/jdk1.7.0_05
+module load gcc/4.7.2
+module load R/3.1.0
+module load picard/1.118
+module load samtools/1.2
+module load git/2.3.3
+module load coreutils/8.9
+
+module load python/2.7.9
+module load cufflinks/2.2.1
+
+cd $AGGREGATION_FOLDER
+
+export LIBRARY_NAME=LN${LIBRARY}
+export REF_DIR=$(dirname "$GENOME_INDEX")
+export SCRIPT_DIR="$STAMPIPES/scripts/tophat"
+
+BAM_COUNT=`ls $BAM_FILES | wc -l`
+
+JOB_BASENAME=".AGG#${AGGREGATION_ID}"
+
+export FINAL_BAM=${LIBRARY_NAME}.all.bam
+
+if [ -n "$REDO_AGGREGATION" ]; then
+  # TODO: Make
+  bash $STAMPIPES/scripts/tophat/aggregate/basic/reset.bash
+fi
+
+MERGE_DUP_JOBNAME=${JOB_BASENAME}_merge_dup
+COUNT_JOBNAME=${JOB_BASENAME}_count
+MAKE_JOBNAME=${JOB_BASENAME}_make
+
+# Check out files match first
+python3 $STAMPIPES/scripts/utility/md5check.py bamfiles.txt || exit 1
+
+PROCESSING=""
+
+if [ ! -e ${FINAL_BAM} ]; then
+
+  PROCESSING="$PROCESSING,${MERGE_DUP_JOBNAME}"
+
+  qsub ${SUBMIT_SLOTS} -N "${MERGE_DUP_JOBNAME}" -V -cwd -S /bin/bash > /dev/stderr << __MERGE__
+    set -x -e -o pipefail
+
+    TMPBAM="\$TMPDIR/$LIBRARY_NAME.bam"
+
+    echo "Hostname: "
+    hostname
+
+    echo "START: "
+    date
+
+    $STAMPIPES/scripts/tophat/merge_or_copy_bam.sh \$TMPBAM  $BAM_FILES
+
+    java -Xmx24g -jar \$(which MarkDuplicates.jar) \
+      INPUT=\$TMPBAM \
+      METRICS_FILE=$LIBRARY_NAME.dups.txt \
+      OUTPUT=$FINAL_BAM \
+      REMOVE_DUPLICATES=false \
+      ASSUME_SORTED=true
+
+    echo "FINISH: "
+    date
+
+__MERGE__
+
+fi
+
+# Currently single-threaded
+make_strand(){
+  name=$1
+  targets="$name.bam $name.starch $name.bw $name.bam.bai"
+  makefile="$STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk"
+
+  if ! make -q -f $makefile $targets ; then
+    PROCESSING="$PROCESSING,${MAKE_JOBNAME}-$name"
+    qsub -N "${MAKE_JOBNAME}-$name" -hold_jid ${MERGE_DUP_JOBNAME} -V -cwd -S /bin/bash > /dev/stderr << __STRAND__
+      set -x -e -o pipefail
+
+      echo "Hostname: "
+      hostname
+
+      echo "START: "
+      date
+
+      make \
+        -f $STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk \
+        $targets
+
+      echo "FINISH: "
+      date
+
+__STRAND__
+  fi
+}
+
+do_cufflinks(){
+  targets="${LIBRARY_NAME}_cufflinks/finished.txt"
+  makefile="$STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk"
+
+  if ! make -q -f $makefile $targets ; then
+
+    jobname="${MAKE_JOBNAME}-cufflinks" 
+    PROCESSING="$PROCESSING,$jobname"
+    tmpbamlink="$LIBRARY_NAME.all.$GENOME.bam"
+
+    export SAMPLE_NAME=$LIBRARY_NAME
+    qsub -N $jobname -hold_jid ${MERGE_DUP_JOBNAME} -V -cwd -S /bin/bash > /dev/stderr -pe threads 2-4 << __CUFFLINKS__
+    set -x -e -o pipefail
+
+    echo "Hostname: "
+    hostname
+
+    echo "START: "
+    date
+
+    rm -f $tmpbamlink
+    ln -s $FINAL_BAM  $tmpbamlink
+
+    make \
+      -f $STAMPIPES/makefiles/tophat/tophat_and_cufflinks.mk \
+      $targets
+
+    rm $tmpbamlink
+
+    echo "FINISH: "
+    date
+
+__CUFFLINKS__
+  fi
+}
+
+## Main ##
+
+make_strand $LIBRARY_NAME.all
+make_strand $LIBRARY_NAME.pos
+make_strand $LIBRARY_NAME.neg
+
+do_cufflinks
+
+
+if [ -n ${PROCESSING} ]; then
+
+  qsub -N "${JOB_BASENAME}_complete" -hold_jid "${PROCESSING}" -V -cwd -S /bin/bash > /dev/stderr << __SCRIPT__
+  set -x -e -o pipefail
+
+  echo "Hostname: "
+  hostname
+
+  echo "START: "
+  date
+
+  bash $STAMPIPES/scripts/tophat/aggregate/basic/checkcomplete.bash
+  bash $STAMPIPES/scripts/tophat/aggregate/basic/attachfiles.bash
+
+  echo "FINISH: "
+  date
+
+__SCRIPT__
+
+fi
