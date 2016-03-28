390b81888d71e54a286352a698358e85971716bf
Jemma Nelson <nelsonjs@uw.edu>
2016-03-28 12:26:39 -0700

    Fixup post_libraries.sh, fix up a few silly bugs
    (Whoops)
    
    
    Former-commit-id: ad49a710395c7b54b80f362375ac33862ac14050

--- scripts/customer/post_libraries.sh
+++ scripts/customer/post_libraries.sh
@@ -21,20 +21,21 @@ for libnum in "$@"; do
 
   libnum=${libnum/LN/}
 
-  lane_ids=( $(lims_get_all "flowcell_lane/?library__number=$libnum&warn=False&failed=False&flowcell__failed=False" | jq -r .id ))
+  lane_ids=$(lims_get_all "flowcell_lane/?library__number=$libnum&warn=False&failed=False&flowcell__failed=False" | jq -r .id )
 
   echo "# LN$libnum - $(wc -w <<< $lane_ids) lanes"
 
-  for lane_id in "${lane_ids[@]}"; do
+  for lane_id in $lane_ids; do
     echo " # lane $lane_id"
     lane_info=$(lims_get "flowcell_lane/$lane_id/")
     content_type=$(jq -r .object_content_type <<< $lane_info)
     lane_path=$(lims_get_all "directory/?purpose=$fastq_purpose&content_type=$content_type&object_id=$lane_id" | jq -r .path )
     flowcell="FC$(jq -r .flowcell_label <<< $lane_info )"
     project_url=$(jq -r .project <<< $lane_info )
-    dist_dir="$CUSTOMER_DISTRIBUTION/$(lims_get_by_url $project_url | jq -r .distribution_directory)"
+    cust_dir="$(lims_get_by_url $project_url | jq -r .distribution_directory)"
+    dist_dir="$CUSTOMER_DISTRIBUTION/$cust_dir"
 
-    if [[ ! -d $dist_dir ]] ;then
+    if [[ -z "$cust_dir" || ! -d $dist_dir ]] ;then
       die "Error: Distribution directory '$dist_dir' for project '$project_url' - library '$libnum,' lane '$lane_id', does not exist" >&2
     fi
     if [[ ! -d $lane_path ]] ;then
@@ -42,7 +43,7 @@ for libnum in "$@"; do
     fi
 
     echo "   " mkdir -p "$dist_dir/$flowcell"
-    echo "   " ln -s "$lane_path" "$dist_dir/flowcell"
+    echo "   " ln -s "$lane_path" "$dist_dir/$flowcell"
   done
   echo
 
