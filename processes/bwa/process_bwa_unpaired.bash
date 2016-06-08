c3441f70ee29933362228fe363a0660a25b33b22
Jemma Nelson <jemma.s.nelson@gmail.com>
2016-06-08 13:20:34 -0700

    Fix alignment uploading.

--- processes/bwa/process_bwa_unpaired.bash
+++ processes/bwa/process_bwa_unpaired.bash
@@ -278,7 +278,12 @@ qsub -hold_jid ${PROCESSING} -N ".com${JOB_BASENAME}" -V -cwd -S /bin/bash > /de
     --finish_alignment
 
   #bash $STAMPIPES/scripts/bwa/attachfiles.bash
-  $ATTACH_ALIGNMENT --attach_file ${SAMPLE_NAME}.sorted.bam --attach_file_purpose all-alignments-bam --attach_file_type bam
+  python3 $STAMPIPES/scripts/lims/upload_data.py \
+    --attach_file_contenttype SequencingData.flowcelllanealignment \
+    --attach_file_objectid ${ALIGNMENT_ID} \
+    --attach_file ${SAMPLE_NAME}.sorted.bam \
+    --attach_file_purpose all-alignments-bam \
+    --attach_file_type bam
 
   rm -r $ALIGN_DIR/fastq
 
