32e08c222e29febd1f3b6bf7b618dbf0a4a4984a
arsnishida <anishida@altiusinstitute.org>
2017-01-11 16:51:31 -0800

    Add files via upload
    Changed flowcell setup:
    - Flowcell processing (barcodes, demultiplexing, etc.) is performed on queue2 instead of default queue0. Alignment and other downstream steps will remain on queue0 but more nodes can be put into queue2 in the future to accomodate. This is something of a hotfix in lieu of setting up slurm priorities.
    - Changed collation dependency from '.cl*' to '.clDS*' since other people's jobs like 'job.cleanup' were getting flagged as dependencies.
    - Removed qstat calls from DMX step.

--- scripts/flowcells/setup.sh
+++ scripts/flowcells/setup.sh
@@ -121,6 +121,7 @@ Workflow,GenerateFASTQ
 
 [Data]
 SampleID,SampleName,index,index2
+none,none,GGGGGGGG,GGGGGGGG
 __SHEET__
 
 if [ -z "$demux" ] ; then
@@ -170,7 +171,7 @@ fi
 case $run_type in
 "NextSeq 500")
     echo "Regular NextSeq 500 run detected"
-    parallel_env="-pe threads 4-8"
+    parallel_env="-pe threads 8"
     link_command="python3 $STAMPIPES/scripts/flowcells/link_nextseq.py -i fastq -o ."
     samplesheet="SampleSheet.csv"
     fastq_dir="$illumina_dir/fastq"  # Lack of trailing slash is important for rsync!
@@ -207,7 +208,7 @@ _U_
     ;;
 "HiSeq 4000")
     echo "Hiseq 4000 run detected"
-    parallel_env="-pe threads 4-8"
+    parallel_env="-pe threads 8"
     link_command="python3 $STAMPIPES/scripts/flowcells/link_nextseq.py -i fastq -o ."
     samplesheet="SampleSheet.csv"
     fastq_dir="$illumina_dir/fastq"  # Lack of trailing slash is important for rsync!
@@ -269,31 +270,33 @@ _U_
     #TODO: Add HISEQ V3 on hiseq 2500 (rapid run mode)
 "HISEQ V4")
     echo "Regular HiSeq 2500 run detected"
-    parallel_env=""
-    link_command='#no linking to do'
-    samplesheet=$(pwd)/Data/Intensities/BaseCalls/SampleSheet.csv
-    mkdir -p $(dirname "$samplesheet")
-    make_hiseq_samplesheet > "$samplesheet"
-    fastq_dir="$illumina_dir/Unaligned/"  # Trailing slash is important for rsync!
-    bc_flag="--hiseq"
-    bcl_tasks=1
+    echo "HiSeq 2500 processing not supported on the new cluster! (Does not have old version of bcl2fastq)"
+    exit 2
+    #parallel_env=""
+    #link_command='#no linking to do'
+    #samplesheet=$(pwd)/Data/Intensities/BaseCalls/SampleSheet.csv
+    #mkdir -p $(dirname "$samplesheet")
+    #make_hiseq_samplesheet > "$samplesheet"
+    #fastq_dir="$illumina_dir/Unaligned/"  # Trailing slash is important for rsync!
+    #bc_flag="--hiseq"
+    #bcl_tasks=1
 
-    set +e
-    read -d '' unaligned_command <<_U_
-    if [ ! -e "$fastq_dir" ] ; then
-            configureBclToFastq.pl \\\\
-              --mismatches "$mismatches" \\\\
-              --output-dir "$fastq_dir" \\\\
-              --fastq-cluster-count 16000000 \\\\
-              --with-failed-reads --sample-sheet $samplesheet \\\\
-              --use-bases-mask "$bcl_mask"  \\\\
-              --input-dir "$illumina_dir/Data/Intensities/BaseCalls"
-    fi
+    #set +e
+    #read -d '' unaligned_command <<_U_
+    #if [ ! -e "$fastq_dir" ] ; then
+    #        configureBclToFastq.pl \\\\
+    #          --mismatches "$mismatches" \\\\
+    #          --output-dir "$fastq_dir" \\\\
+    #          --fastq-cluster-count 16000000 \\\\
+    #          --with-failed-reads --sample-sheet $samplesheet \\\\
+    #          --use-bases-mask "$bcl_mask"  \\\\
+    #          --input-dir "$illumina_dir/Data/Intensities/BaseCalls"
+    #fi
 
-    cd "$fastq_dir"
-    qmake -now no -cwd -q all.q -V -- -j "$NODES"
-_U_
-    set -e
+    #cd "$fastq_dir"
+    #qmake -now no -cwd -q all.q -V -- -j "$NODES"
+#_U_
+    #set -e
     ;;
 *)
     echo "Unrecognized run type '$run_type'"
@@ -322,8 +325,7 @@ cat > run_bcl2fastq.sh <<__BCL2FASTQ__
 #!/bin/bash
 
 source $MODULELOAD
-module load bcl2fastq/1.8.4
-module load bcl2fastq2/2.15.0.4
+module load bcl2fastq2/2.17.1.14
 source $PYTHON3_ACTIVATE
 
 source $STAMPIPES/scripts/lims/api_functions.sh
@@ -347,7 +349,7 @@ lims_patch "flowcell_run/$flowcell_id/" "folder_name=${PWD##*/}"
 # Submit a barcode job for each mask
 for bcmask in $(python $STAMPIPES/scripts/flowcells/barcode_masks.py | xargs) ; do
     export bcmask
-    qsub -cwd -N "bc-$flowcell" -q queue2 -pe threads 4-8 -V -S /bin/bash <<'__BARCODES__'
+    qsub -cwd -N "bc-$flowcell" -q queue2 -pe threads 8 -V -S /bin/bash <<'__BARCODES__'
     GOMAXPROCS=\$(( NSLOTS * 2 )) bcl_barcode_count --mask=\$bcmask $bc_flag > barcodes.\$bcmask.json
 
     python3 $STAMPIPES/scripts/lims/upload_data.py --barcode_report barcodes.\$bcmask.json
@@ -401,7 +403,7 @@ python3 "$STAMPIPES/scripts/laneprocess.py" \
 
 bash collate.bash
 
-qsub -N .run$flowcell -q queue2 -hold_jid '.clDS*' -cwd -V -q all.q -S /bin/bash <<__ALIGN__
+qsub -N .run$flowcell -q queue2 -hold_jid '.clDS*' -cwd -V -S /bin/bash <<__ALIGN__
   bash fastqc.bash
 
   # Create alignment scripts
