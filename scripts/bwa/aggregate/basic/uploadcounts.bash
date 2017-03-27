255f05b44057b8d3872d1204b05c1f31771aa86c
Jemma Nelson <nelsonjs@altiusinstitute.org>
2017-03-27 13:28:20 -0700

    Upload hotspot2 stats for aggregation
    Former-commit-id: d3d225a913b01eb191116127ac9b5e52a37015f5

--- scripts/bwa/aggregate/basic/uploadcounts.bash
+++ scripts/bwa/aggregate/basic/uploadcounts.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
 
 module load openssl-dev/1.0.1t
 module load python/2.7.11
 
+# Upload regular stats
 python2 "$STAMPIPES/scripts/lims/upload_aggregation_stats.py" \
   --aggregation "$AGGREGATION_ID" \
   --counts_file "$LIBRARY_NAME.tagcounts.txt"
+
+# Upload SPOT stats
+python2 "$STAMPIPES/scripts/lims/upload_aggregation_stats.py" \
+  --aggregation "$AGGREGATION_ID" \
+  --counts_file "$LIBRARY_NAME.$GENOME.uniques.sorted.hotspot2.info"
