7b7e6ea76afc9257a2f0cb328d25012f7d8ad07f
Jemma Nelson <nelsonjs@altiusinstitute.org>
2018-06-25 16:06:59 -0700

    Modify bash process to use nextflow
    Former-commit-id: 8a1e43ce1b3f63270ff221c585bf8c7f21a7ff86

--- processes/bwa/process_bwa_paired_trimmed.bash
+++ processes/bwa/process_bwa_paired_trimmed.bash
@@ -1,7 +1,84 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 # Dependencies
 source "$MODULELOAD"
+module purge
+module load jdk
+module load nextflow
+module load python3
+
+adapterfile="adapters.txt"
+
+outdir="output"
+workdir="work"
+
+# Remove old stuff if necessary
+if [[ -n "$REDO_ALIGNMENT" ]] ; then
+  rm -rf "$outdir"
+  rm -rf "$workdir"
+  rm -rf "$adapterfile"
+  python3 "$STAMPIPES/scripts/lims/upload_data.py" \
+     --clear_align_stats \
+     --alignment_id "$ALIGNMENT_ID"
+fi
+
+# Set up adapters
+if [[ (-n "$ADAPTER_P7") && (-n "$ADAPTER_P5") ]]; then
+  tmpfile=$(mktemp)
+  echo -e "P7\t$ADAPTER_P7\nP5\t$ADAPTER_P5" > "$tmpfile"
+  rsync --checksum "$tmpfile" "$adapterfile"
+  rm "$tmpfile"
+fi
+
+# Tell LIMS we're starting alignment
+echo \
+python3 "$STAMPIPES/scripts/lims/upload_data.py" \
+  -a "$LIMS_API_URL" \
+  -t "$LIMS_API_TOKEN" \
+  --alignment_id "$ALIGNMENT_ID" \
+  --start_alignment_progress \
+  --adapter_file "$ADAPTER_FILE" \
+  --version_file "$VERSION_FILE"
+
+# Run the whole process
+nextflow run \
+  "$STAMPIPES/processes/bwa/process_bwa_paired_trimmed.nf" \
+  -c "$STAMPIPES/nextflow.config" \
+  -w "$workdir" \
+  --r1 "$R1_FASTQ" \
+  --r2 "$R2_FASTQ" \
+  --adapter_file adapters.txt  \
+  --genome "$BWAINDEX" \
+  --outdir "$outdir" \
+  --threads 3 \
+  -profile cluster \
+  -with-report nextflow.report.html \
+  -with-dag nextflow.flowchart.html \
+  -with-timeline nextflow.timeline.html \
+  -resume
+
+# Upload results
+echo \
+python3 "$STAMPIPES/scripts/lims/upload_data.py" \
+  -a "$LIMS_API_URL" \
+  -t "$LIMS_API_TOKEN" \
+  -f "$FLOWCELL" \
+  --alignment_id "$ALIGNMENT_ID" \
+  --flowcell_lane_id "$FLOWCELL_LANE_ID" \
+  --spotfile "$outdir/subsample.spot.out" \
+  --countsfile "$outdir/tagcounts.txt" \
+  --insertsfile "$outdir/CollectInsertSizeMetrics.picard" \
+  --dupsfile "$outdir/MarkDuplicates.picard" \
+  --finish_alignment
+  #--spotdupfile ${SAMPLE_NAME}.rand.uniques.sorted.spotdups.txt \
+
+# TODO: Delete below this line
+################################## Exit ##################################
+                                   exit
+##########################################################################
+
 module load zlib/1.2.8
 module load bedops/2.4.19
 module load bedtools/2.25.0
