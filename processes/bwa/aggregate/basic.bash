4aeb091b86d0eaf9d831532c9f1e98749c7ed6ac
Jemma Nelson <nelsonjs@altius.org>
2019-12-17 15:44:41 -0800

    Restrict modules loaded for nextflow
    This is done primarily to fix macs2 for ChIP-Seq, which was previously
    using the wrong python.

--- processes/bwa/aggregate/basic.bash
+++ processes/bwa/aggregate/basic.bash
@@ -7,8 +7,6 @@ cd "$(dirname "$0")"
 
 source "$MODULELOAD"
 module purge
-module load jdk
-module load nextflow
 module load python/3.5.1
 module load anaconda/2.1.0-dev
 
@@ -60,28 +58,32 @@ if [[ -z "$PEAK_CALLER" ]] ; then
 fi
 
 # Run the whole process
-nextflow run \
-  "$STAMPIPES/processes/bwa/aggregate/basic.nf" \
-  -c "$STAMPIPES/nextflow.config" \
-  -w "$workdir" \
-  --bams "$bamfiles" \
-  --genome "$GENOME_INDEX" \
-  --mappable "$MAPPABLE_REGIONS" \
-  --chrom_sizes "$CHROM_SIZES" \
-  --peakcaller "$PEAK_CALLER" \
-  --centers "$CENTER_SITES" \
-  --chrom_bucket "$CHROM_BUCKET" \
-  --hotspot_index "$HOTSPOT_INDEX" \
-  --hotspot_id "AG$AGGREGATION_ID" \
-  --bias "$STAMPIPES_DATA/footprints/vierstra_et_al.txt" \
-  --UMI="$UMI" \
-  --outdir "$outdir" \
-  --threads 3 \
-  -profile cluster,modules \
-  -with-report nextflow.report.html \
-  -with-dag nextflow.flowchart.html \
-  -with-timeline nextflow.timeline.html \
-  -resume
+(
+  module purge
+  module load jdk nextflow
+  nextflow run \
+    "$STAMPIPES/processes/bwa/aggregate/basic.nf" \
+    -c "$STAMPIPES/nextflow.config" \
+    -w "$workdir" \
+    --bams "$bamfiles" \
+    --genome "$GENOME_INDEX" \
+    --mappable "$MAPPABLE_REGIONS" \
+    --chrom_sizes "$CHROM_SIZES" \
+    --peakcaller "$PEAK_CALLER" \
+    --centers "$CENTER_SITES" \
+    --chrom_bucket "$CHROM_BUCKET" \
+    --hotspot_index "$HOTSPOT_INDEX" \
+    --hotspot_id "AG$AGGREGATION_ID" \
+    --bias "$STAMPIPES_DATA/footprints/vierstra_et_al.txt" \
+    --UMI="$UMI" \
+    --outdir "$outdir" \
+    --threads 3 \
+    -profile cluster,modules \
+    -with-report nextflow.report.html \
+    -with-dag nextflow.flowchart.html \
+    -with-timeline nextflow.timeline.html \
+    -resume
+)
 
 export PEAK_CALLER
 ( cd "$outdir" \
