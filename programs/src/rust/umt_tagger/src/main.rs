c423f86003a6f00c44e5f2deb715462050b15e45
Jemma Nelson <nelsonjs@altius.org>
2021-06-24 11:19:39 -0700

    Improve error handling messages

--- programs/src/rust/umt_tagger/src/main.rs
+++ programs/src/rust/umt_tagger/src/main.rs
@@ -1,5 +1,5 @@
-use std::path::PathBuf;
 use rust_htslib::{bam, bam::Read, tpool};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// Opt contains all of our configuration for this program
@@ -19,7 +19,12 @@ struct Opt {
     /// 0: No compression (similar to `samtools view -u`)
     /// 1: Fastest compression
     /// 9: Best compression
-    #[structopt(short = "z", long = "compression", value_name = "level", default_value = "6")]
+    #[structopt(
+        short = "z",
+        long = "compression",
+        value_name = "level",
+        default_value = "6"
+    )]
     compression_level: u32,
 
     /// Path to the input BAM file.
@@ -41,27 +46,23 @@ struct Opt {
     tagname: String,
 }
 
-
 /// Run the program, all configuration specified in options
-fn run(options: Opt) {
+fn run(options: Opt) -> Result<(), Box<dyn std::error::Error>> {
     let mut bam = match options.input {
-        Some(path) => bam::Reader::from_path(path).expect("Could not open input file"),
-        None => bam::Reader::from_stdin().expect("Could not open stdin"),
+        Some(path) => bam::Reader::from_path(path)?,
+        None => bam::Reader::from_stdin()?,
     };
 
     let header = bam::Header::from_template(bam.header());
     let mut output = match options.output {
-        Some(path) => bam::Writer::from_path(path, &header, bam::Format::BAM).expect("Could not open output file"),
-        None => bam::Writer::from_stdout(&header, bam::Format::BAM).expect("Could not open stdout"),
+        Some(path) => bam::Writer::from_path(path, &header, bam::Format::BAM)?,
+        None => bam::Writer::from_stdout(&header, bam::Format::BAM)?,
     };
 
-    let threadpool = tpool::ThreadPool::new(options.threads).expect("Could not create threadpool");
-    bam.set_thread_pool(&threadpool)
-        .expect("Could not modify input threadpool");
-    output.set_thread_pool(&threadpool)
-        .expect("Could not modify output threadpool");
-    output.set_compression_level(bam::CompressionLevel::Level(options.compression_level))
-        .expect("Could not set output compression level");
+    let threadpool = tpool::ThreadPool::new(options.threads)?;
+    bam.set_thread_pool(&threadpool)?;
+    output.set_thread_pool(&threadpool)?;
+    output.set_compression_level(bam::CompressionLevel::Level(options.compression_level))?;
 
     let tagname = options.tagname.as_bytes();
     let separator = options.separator as u8;
@@ -71,8 +72,9 @@ fn run(options: Opt) {
         result.expect("BAM parsing failed");
 
         move_tag(&mut record, tagname, separator);
-        output.write(&record).expect("Could not write read");
+        output.write(&record)?;
     }
+    Ok(())
 }
 
 /// Checks for the presence of the UMT separator in the read name
@@ -86,7 +88,7 @@ fn move_tag(record: &mut bam::Record, tagname: &[u8], separator: u8) {
         let sep_location = owned.len() - x - 1;
 
         let new_name = &owned[..sep_location];
-        let umt = &owned[sep_location+1..];
+        let umt = &owned[sep_location + 1..];
         record.set_qname(new_name);
         record.push_aux(tagname, &bam::record::Aux::String(umt))
     }
@@ -104,5 +106,13 @@ fn main() {
         opt.threads = 1;
     }
 
-    run(opt);
+    let result = run(opt);
+
+    std::process::exit(match result {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("error: {:}", err);
+            1
+        }
+    });
 }
