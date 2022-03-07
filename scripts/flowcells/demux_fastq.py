ece30ed8db657a65e88a1c484695830553dc309b
Jemma Nelson <nelsonjs@altiusinstitute.org>
2022-03-06 16:30:16 -0800

    Cleanup demux_fastq.py
    Mostly just fixing warnings & formatting
    also add --debug option

--- scripts/flowcells/demux_fastq.py
+++ scripts/flowcells/demux_fastq.py
@@ -1,47 +1,64 @@
 # This is a quick script to split up FASTQ files by barcode given
 # Used to rescue tags from undeterminde state
 
-import sys, os, re, operator, subprocess
+import argparse
+import errno
+import itertools
+import json
+import logging
+import operator
+import os
+import re
+import subprocess
+import sys
+
 from Bio import SeqIO
 from Bio.SeqIO.QualityIO import FastqGeneralIterator
-import logging
-
-import argparse
-
-import errno
 
 log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
+lengths = set([])
+
 def parseArgs():
-    parser = argparse.ArgumentParser(description='Split up fastq files by barcode')
-    parser.add_argument('--mismatches', type=int, default=0, help='number of mismatches')
-    parser.add_argument('--processing', dest='processing_file', action='store', required=True,
-            help='processing.json to use (mandatory)')
-    parser.add_argument('--suffix', dest='suffix', default='', help='suffix to add to sample names')
-    parser.add_argument('--lane', dest='lane', type=int, default=1, help='Lane to process (default 1)')
-    parser.add_argument('--autosuffix', action="store_true", default=False, help='Automatically guess a suffix name')
-    parser.add_argument('--outdir', dest='outdir', default='.', help='Output directory')
-    parser.add_argument('--dry-run', dest='dry_run', default=False, action='store_true', help='Do not actually demultiplex')
-    parser.add_argument('--ignore_failed_lanes', default=False, action="store_true", help="Ignore any lanes marked as failed in processing")
+    parser = argparse.ArgumentParser(
+        description='Split up fastq files by barcode')
+    parser.add_argument('--mismatches', type=int, default=0,
+                        help='number of mismatches')
+    parser.add_argument('--processing', dest='processing_file', required=True,
+                        help='processing.json to use (mandatory)')
+    parser.add_argument('--suffix', dest='suffix', default='',
+                        help='suffix to add to sample names')
+    parser.add_argument('--lane', dest='lane', type=int,
+                        default=1, help='Lane to process (default 1)')
+    parser.add_argument('--autosuffix', action="store_true",
+                        default=False, help='Automatically guess a suffix name')
+    parser.add_argument('--outdir', dest='outdir',
+                        default='.', help='Output directory')
+    parser.add_argument('--dry-run', dest='dry_run', default=False,
+                        action='store_true', help='Do not actually demultiplex')
+    parser.add_argument('--ignore_failed_lanes', default=False, action="store_true",
+                        help="Ignore any lanes marked as failed in processing")
+    parser.add_argument('--debug', dest="debug", action="store_true",
+                        help="Output debugging info")
     parser.add_argument('infile', nargs='+')
 
     args = parser.parse_args()
     return args
 
 
-
-import itertools
-# Generator for mismatches
-# returns original string + whatever variations have at most [mismatches] hamming distance
 def mismatch(word, mismatches):
+    """ Generator for mismatches
+    returns original string + whatever variations have at most
+    [mismatches] hamming distance """
     for d in range(mismatches+1):
         for locs in itertools.combinations(range(len(word)), d):
-            thisWord = [[char] for char in word]
+            this_word = [[char] for char in word]
             for loc in locs:
-                origChar = word[loc]
-                thisWord[loc] = [l for l in "ACGTN" if l != origChar]
-            for poss in itertools.product(*thisWord):
+                orig_char = word[loc]
+                this_word[loc] = [base for base in "ACGTN" if base != orig_char]
+            for poss in itertools.product(*this_word):
                 yield "".join(poss)
+
 
 def guess_suffix(filename):
     file = os.path.basename(filename)
@@ -61,7 +78,7 @@ def guess_suffix(filename):
 
     match = nextseq_format.search(file)
     if match:
-        suffix = "_R%s_%s" % ( match.group('read'), match.group('lane'))
+        suffix = "_R%s_%s" % (match.group('read'), match.group('lane'))
         return suffix
 
     hiseq_format = re.compile(r"""
@@ -83,10 +100,11 @@ def guess_suffix(filename):
 
     return ""
 
-import json
+
 def parse_processing_file(file, mismatches, suffix, lane, outdir, ignore_failed_lanes=False):
-    barcodes = dict()
-    labels = dict()
+    """ Decode processing file into barcode->file assignments """
+    barcodes = {}
+    labels = {}
     with open(file) as data_file:
         data = json.load(data_file)
 
@@ -95,14 +113,15 @@ def parse_processing_file(file, mismatches, suffix, lane, outdir, ignore_failed_
     if run_type == "NextSeq 500":
         lane_libraries = data['libraries']
     elif run_type == "HISEQ V4":
-        lane_libraries = [ l for l in data['libraries'] if l['lane'] == lane ]
+        lane_libraries = [lib for lib in data['libraries'] if lib['lane'] == lane]
     elif run_type == "HiSeq 4000":
-        lane_libraries = [ l for l in data['libraries'] if l['lane'] == lane ]
+        lane_libraries = [lib for lib in data['libraries'] if lib['lane'] == lane]
     # TODO: Is this always correct?
     elif run_type.startswith("Novaseq 6000"):
-        lane_libraries = [ l for l in data['libraries'] if l['lane'] == lane ]
+        lane_libraries = [lib for lib in data['libraries'] if lib['lane'] == lane]
     else:
-        logging.warn("Run type %s not supported; using all libraries" % run_type)
+        logging.warn(
+            "Run type %s not supported; using all libraries" % run_type)
         lane_libraries = data['libraries']
 
     for library in lane_libraries:
@@ -110,16 +129,18 @@ def parse_processing_file(file, mismatches, suffix, lane, outdir, ignore_failed_
         if library.get('alignments', []):
             label = library['alignments'][0]['sample_name']
         else:
-            label = "%s_%s_L%03d" % ( library['samplesheet_name'], library['barcode_index'], library['lane'] )
+            label = "%s_%s_L%03d" % (
+                library['samplesheet_name'], library['barcode_index'], library['lane'])
 
         if ignore_failed_lanes and library["failed"]:
-            logging.info("Ignoring failed library %s" % label) 
+            logging.info("Ignoring failed library %s" % label)
             continue
 
         project_dir = "Project_%s" % library['project']
         sample_dir = "Sample_%s" % library['samplesheet_name']
         library_dir = os.path.join(outdir, project_dir, sample_dir)
-        outfile_name = os.path.join(library_dir, "%s%s.fastq.gz" % (label, suffix))
+        outfile_name = os.path.join(
+            library_dir, "%s%s.fastq.gz" % (label, suffix))
 
         try:
             os.makedirs(library_dir)
@@ -131,30 +152,34 @@ def parse_processing_file(file, mismatches, suffix, lane, outdir, ignore_failed_
         barcode1 = barcode_indices[0]
         barcode2 = barcode_indices[1] if len(barcode_indices) > 1 else ""
 
-        lengths.add(( len(barcode1), len(barcode2) ))
+        lengths.add((len(barcode1), len(barcode2)))
 
         for b1 in mismatch(barcode1, mismatches):
             for b2 in mismatch(barcode2, mismatches):
                 barcode = (b1, b2)
                 # TODO: This can be smarter
                 if barcode in barcodes:
-                    logging.error("Barcode %s already taken, lower --mismatches! (taken by %s+%s)" % (barcode, barcode1, barcode2))
+                    logging.error(
+                        "Barcode %s already taken, lower --mismatches! (taken by %s+%s)" % (barcode, barcode1, barcode2))
                     sys.exit(1)
                 barcodes[barcode] = label
 
-        labels[label] = { "filtered": 0, "unfiltered": 0, "total": 0 }
+        labels[label] = {"filtered": 0, "unfiltered": 0, "total": 0}
         # TODO: Warning! this will overwrite files!
         outfile = open(outfile_name, 'wb')
         labels[label]["fh"] = outfile
-        labels[label]["out"] = subprocess.Popen(['gzip', '-7'], stdout=outfile, stdin=subprocess.PIPE)
+        labels[label]["out"] = subprocess.Popen(
+            ['gzip', '-7'], stdout=outfile, stdin=subprocess.PIPE)
 
-    logging.info("Mapping %d barcodes to %s libraries" % (len(barcodes), len(lane_libraries)))
+    logging.info("Mapping %d barcodes to %s libraries" %
+                 (len(barcodes), len(lane_libraries)))
     logging.debug(barcodes)
 
     return barcodes, labels
 
-def split_file(filename, barcodes, labels):
 
+def split_file(filename, barcodes, labels):
+    """ Demultiplex a file """
     barcode_re = re.compile(r"""
             [012]:                  #
             ([YN]):                 # Fail/pass chastity filtering
@@ -169,9 +194,11 @@ def split_file(filename, barcodes, labels):
     logging.info("Demultiplexing file: %s" % filename)
 
     if filename.endswith('.gz'):
-        parsein = subprocess.Popen(['zcat', filename], stdout=subprocess.PIPE, universal_newlines=True)
+        parsein = subprocess.Popen(
+            ['zcat', filename], stdout=subprocess.PIPE, universal_newlines=True)
     else:
-        parsein = subprocess.Popen(['cat', filename], stdout=subprocess.PIPE, universal_newlines=True)
+        parsein = subprocess.Popen(
+            ['cat', filename], stdout=subprocess.PIPE, universal_newlines=True)
 
     for record, seq, qual in FastqGeneralIterator(parsein.stdout):
         tally += 1
@@ -184,42 +211,49 @@ def split_file(filename, barcodes, labels):
             sys.exit(1)
 
         matches = match.groups()
-        filter = matches[0]
+        filter_str = matches[0]
         barcode1 = matches[1]
-        if (len(matches) > 2):
+        barcode2 = ""
+        label = None
+        if len(matches) > 2:
             barcode2 = matches[2]
-        if barcode2 == None:
-            barcode2 = ""
-        matched=False
-        for format in lengths:
-            barcode = ( barcode1[:format[0]], barcode2[:format[1]] )
+            if barcode2 is None:
+                barcode2 = ""
+        matched = False
+        for fmt in lengths:
+            barcode = (barcode1[:fmt[0]], barcode2[:fmt[1]])
             if barcode in barcodes:
                 label = barcodes[barcode]
-                matched=True
+                matched = True
                 break
 
         if matched:
             labels[label]['total'] += 1
 
             # Replace recorded barcode
-            sepIndex = record.rfind(':')
-            record = record[:sepIndex + 1] + barcode1 + "+" + barcode2
-            #write to FASTQ
-            text = bytes('@' + record + '\n' + seq + '\n+\n' + qual +'\n', 'UTF-8')
+            sep_index = record.rfind(':')
+            record = record[:sep_index + 1] + barcode1 + "+" + barcode2
+            # write to FASTQ
+            text = bytes('@' + record + '\n' + seq +
+                         '\n+\n' + qual + '\n', 'UTF-8')
             labels[label]['out'].stdin.write(text)
 
-            if filter == "Y":
+            if filter_str == "Y":
                 labels[label]["filtered"] += 1
             else:
                 labels[label]['unfiltered'] += 1
 
+    # Wait for all subprocesses to finish
     parsein.communicate()
 
 
 def main(argv):
     args = parseArgs()
 
-    logging.basicConfig(level=logging.INFO, format=log_format)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG, format=log_format)
+    else:
+        logging.basicConfig(level=logging.INFO, format=log_format)
 
     global lengths
     lengths = set([])
@@ -232,15 +266,15 @@ def main(argv):
         args.suffix = guess_suffix(args.infile[0])
         logging.info("--autosuffix, guessing suffix as %s" % args.suffix)
 
-    barcodes, labels = parse_processing_file(args.processing_file, args.mismatches, args.suffix, args.lane, args.outdir, ignore_failed_lanes=args.ignore_failed_lanes)
+    barcodes, labels = parse_processing_file(
+        args.processing_file, args.mismatches, args.suffix, args.lane, args.outdir, ignore_failed_lanes=args.ignore_failed_lanes)
 
     if args.dry_run:
         logging.info("Dry run, exiting")
         sys.exit(0)
 
-
     for filename in args.infile:
-        split_file(filename, barcodes, labels) 
+        split_file(filename, barcodes, labels)
 
     print("Barcode matching tallies:")
 
@@ -250,6 +284,7 @@ def main(argv):
     for label, info in labels.items():
         info['out'].communicate()
         info['fh'].close()
+
 
 if __name__ == "__main__":
     main(sys.argv)
